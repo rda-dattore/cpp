ead98601570177379703bc35302947b5bb86f7d9
rda-dattore <dattore@ucar.edu>
2017-07-27 14:33:36 -0600

    initial commit

---
+++ include/little_r.hpp
@@ -0,0 +1,37 @@
+// FILE: little_r.hpp
+
+#ifndef LITTLE_R_H
+#define LITTLE_R_H
+
+#include <vector>
+#include <iodstream.hpp>
+#include <observation.hpp>
+
+class InputLITTLE_RStream : public idstream
+{
+public:
+  InputLITTLE_RStream() {}
+  int ignore();
+  int peek();
+  int read(unsigned char *buffer,size_t buffer_length);
+};
+
+class LITTLE_RObservation : public Observation
+{
+public:
+  LITTLE_RObservation() : platform_code_(0),data_records_(),is_sounding_(false) {}
+  const std::vector<std::tuple<float,int,float,int,float,int,float,int,float,int,float,int,float,int,float,int,float,int,float,int>>& data_records() const { return data_records_; }
+  void fill(const unsigned char *stream_buffer,bool fill_header_only);
+  short platform_code() const { return platform_code_; }
+  DateTime getDateTime() const;
+  ObservationLocation getLocation() const;
+  bool is_sounding() const { return is_sounding_; }
+  int num_data_records() const { return data_records_.size(); }
+
+protected:
+  short platform_code_;
+  std::vector<std::tuple<float,int,float,int,float,int,float,int,float,int,float,int,float,int,float,int,float,int,float,int>> data_records_;
+  bool is_sounding_;
+};
+
+#endif
