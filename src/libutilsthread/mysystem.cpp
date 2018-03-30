704a01c090dc321eedfc54d8d2c7ee104a433072
rda-dattore <dattore@ucar.edu>
2018-03-30 17:36:15 -0600

    initial commit

---
+++ src/libutilsthread/mysystem.cpp
@@ -0,0 +1,181 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sstream>
+#include <regex>
+#include <thread>
+#include <strutils.hpp>
+#include <utils.hpp>
+
+namespace mysystem_t {
+
+struct ThreadStruct {
+  ThreadStruct() : tid(0),pipe(0),ss(nullptr) {}
+
+  pthread_t tid;
+  int pipe;
+  std::stringstream *ss;
+};
+
+extern "C" void *read_output(void *ts)
+{
+  ThreadStruct *t=(ThreadStruct *)ts;
+  int len;
+  char buffer[32768];
+
+  while ( (len=read(t->pipe,buffer,32768)) > 0) {
+    *(t->ss) << std::string(buffer,len);
+  }
+  close(t->pipe);
+  return nullptr;
+}
+
+extern "C" void *read_error(void *ts)
+{
+  ThreadStruct *t=(ThreadStruct *)ts;
+  int len;
+  char buffer[32768];
+
+  while ( (len=read(t->pipe,buffer,32768)) > 0) {
+    *(t->ss) << std::string(buffer,len);
+  }
+  close(t->pipe);
+  return nullptr;
+}
+
+} // end namespace mysystem_t
+
+namespace unixutils {
+
+std::string retry_command(std::string command,int num_retries)
+{
+  std::stringstream output,error;
+  std::string error_list;
+  int nr=0;
+
+  if (mysystem2(command,output,error) < 0) {
+// deal with some specific situations
+    if (std::regex_search(error.str(),std::regex("^mv: preserving permissions"))) {
+	auto  sp=strutils::split(command);
+	if (sp[0] == "/bin/mv" && std::regex_search(sp[1],std::regex("^/tmp"))) {
+	  error.str("");
+	}
+    }
+    while (!error.str().empty() && nr < num_retries) {
+	if (!error_list.empty()) {
+	  error_list+=";";
+	}
+	error_list+="'"+error.str()+"'";
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	++nr;
+	mysystem2(command,output,error);
+    }
+  }
+  if (nr < num_retries) {
+    error_list="";
+  }
+  return error_list;
+}
+
+int mysystem2(std::string command,std::stringstream& output,std::stringstream& error)
+{
+  output.str("");
+  error.str("");
+  int pipe_out[2],pipe_err[2];
+  pipe(pipe_out);
+  pipe(pipe_err);
+  auto child_pid=fork();
+  if (child_pid < 0) {
+    error << "Unable to fork";
+    return -1;
+  }
+  else if (child_pid == 0) {
+    std::string new_command;
+    char c_quote='\0';
+    auto in_quotes=false;
+    auto sp=strutils::split(command);
+    for (size_t n=0; n < sp.size(); ++n) {
+	if ((sp[n][0] == '"' || sp[n][0] == '\'') && !in_quotes) {
+	    c_quote=sp[n][0];
+	    sp[n]=sp[n].substr(1);
+	    in_quotes=true;
+	}
+	if (in_quotes && sp[n].back() == c_quote) {
+	  in_quotes=false;
+	  strutils::chop(sp[n]);
+	  c_quote='\0';
+	}
+	if (in_quotes) {
+	  new_command+=sp[n]+" ";
+	}
+	else {
+	  new_command+=sp[n]+"<!>";
+	}
+    }
+    strutils::chop(new_command,3);
+    sp=strutils::split(new_command,"<!>");
+    auto argv=new char *[sp.size()+1];
+    auto idx=sp[0].rfind("/");
+    if (idx == std::string::npos) {
+	argv[0]=new char[sp[0].length()+1];
+	strcpy(argv[0],sp[0].c_str());
+    }
+    else {
+	auto arg0=sp[0].substr(idx+1);
+	argv[0]=new char[arg0.length()+1];
+	strcpy(argv[0],arg0.c_str());
+    }
+    size_t last=1;
+    for (size_t n=last; n < sp.size(); ++n) {
+	strutils::replace_all(sp[n],"__INNER_QUOTE__","'");
+	if (sp[n] == "<") {
+	  if (n == (sp.size()-2)) {
+	    auto rstdin=open(sp[n+1].c_str(),O_RDONLY);
+	    dup2(rstdin,0);
+	    close(rstdin);
+	    break;
+	  }
+	  else {
+	    error << "bad input redirection";
+	    return -1;
+	  }
+	}
+	else {
+	  argv[n]=new char[sp[n].length()+1];
+	  strcpy(argv[n],sp[n].c_str());
+	}
+	++last;
+    }
+    argv[last]=NULL;
+    close(pipe_out[0]);
+    close(pipe_err[0]);
+    dup2(pipe_out[1],1);
+    dup2(pipe_err[1],2);
+    execv(sp[0].c_str(),argv);
+    std::cerr << sp[0] << ": command not found" << std::endl;
+    exit(1);
+  }
+  else {
+    close(pipe_out[1]);
+    close(pipe_err[1]);
+    mysystem_t::ThreadStruct tstructs[2];
+    tstructs[0].pipe=pipe_out[0];
+    tstructs[0].ss=&output;
+    pthread_create(&tstructs[0].tid,NULL,mysystem_t::read_output,reinterpret_cast<void *>(&tstructs[0]));
+    tstructs[1].pipe=pipe_err[0];
+    tstructs[1].ss=&error;
+    pthread_create(&tstructs[1].tid,NULL,mysystem_t::read_error,reinterpret_cast<void *>(&tstructs[1]));
+    pthread_join(tstructs[0].tid,NULL);
+    pthread_join(tstructs[1].tid,NULL);
+    int status;
+    auto pid=waitpid(child_pid,&status,0);
+    if (pid < 0 || !error.str().empty()) {
+	return -1;
+    }
+    else {
+	return 0;
+    }
+  }
+}
+
+} // end namespace unixutils
---
+++ src/libutilsthread/remotefileutils.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <regex>
+#include <thread>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <datetime.hpp>
+#include <strutils.hpp>
+#include <utils.hpp>
+
+namespace unixutils {
+
+std::string remote_file(std::string server_name,std::string local_tmpdir,std::string remote_filename,std::string& error)
+{
+  FILE *p;
+  int status;
+  std::string local_filename;
+
+  p=popen(("rsync -aLq "+server_name+":"+remote_filename+" "+local_tmpdir).c_str(),"r");
+  status=pclose(p);
+  if (status == 0) {
+    local_filename=local_tmpdir+remote_filename.substr(remote_filename.rfind("/"));
+  }
+  else {
+    error="rsync returned status "+strutils::itos(status);
+  }
+  return local_filename;
+}
+
+std::string remote_web_file(std::string URL,std::string local_tmpdir)
+{
+  std::string local_filename;
+  size_t num_tries=0;
+  while (local_filename.empty() && num_tries < 3) {
+    local_filename=local_tmpdir+URL.substr(URL.rfind("/"));
+    std::stringstream oss,ess;
+    mysystem2("/bin/sh -c \"curl -q -s -w '%{http_code}\\n' -L -o "+local_filename+" "+strutils::substitute(URL,"%","%25")+"\"",oss,ess);
+    struct stat buf;
+    if (oss.str() != "200\n" || stat(local_filename.c_str(),&buf) != 0 || buf.st_size == 0) {
+	mysystem2("/bin/rm -rf "+local_filename,oss,ess);
+	std::this_thread::sleep_for(std::chrono::seconds(15));
+	local_filename="";
+	++num_tries;
+    }
+  }
+  return local_filename;
+}
+
+int rdadata_sync(std::string directory,std::string relative_path,std::string remote_path,std::string rdadata_home,std::string& error)
+{
+  if (directory.empty()) {
+    error="missing directory";
+    return -1;
+  }
+  if (relative_path.empty()) {
+    error="nothing to copy";
+    return -1;
+  }
+  if (remote_path.empty()) {
+    error="unknown destination";
+    return -1;
+  }
+  int status=0;
+  std::list<std::string> hosts;
+  error="";
+  if (!unixutils::hosts_loaded(hosts,rdadata_home)) {
+    error="unable to get list of host names";
+    return -1;
+  }
+  setreuid(15968,15968);
+  for (const auto& host : hosts) {
+    auto retry_error=retry_command("/bin/sh -c 'cd "+directory+"; rsync -rptgD --relative -e __INNER_QUOTE__ssh -i "+rdadata_home+"/.ssh/"+host+"-sync_rdadata_rsa -l rdadata__INNER_QUOTE__ "+relative_path+" "+host+".ucar.edu:"+remote_path+"'",3);
+    if (!retry_error.empty()) {
+	if (!error.empty()) {
+	  error+=", ";
+	}
+	error+="rsync error on '"+host+"': *'"+retry_error+"'*";
+	status=-1;
+    }
+  }
+  return status;
+}
+
+int rdadata_unsync(std::string remote_filename,std::string rdadata_home,std::string& error)
+{
+// special tokens in remote_filename
+//  __HOST__ will be replaced with hosts.getCurrent()
+//
+  std::list<std::string> hosts;
+  std::string rname;
+  std::string retry_error;
+  int status=0;
+
+  error="";
+  if (!unixutils::hosts_loaded(hosts,rdadata_home)) {
+    error="unable to get list of host names";
+    return -1;
+  }
+  for (auto& host : hosts) {
+    rname=remote_filename;
+    strutils::replace_all(rname,"__HOST__",host);
+    retry_error=retry_command("/bin/sh -c \""+rdadata_home+"/bin/"+host+"-sync -d "+rname+"\"",3);
+    if (retry_error.length() > 0) {
+	if (error.length() > 0) {
+	  error+=", ";
+	}
+	error+=host+"-sync error: *'"+retry_error+"'*";
+	status=-1;
+    }
+  }
+  return status;
+}
+
+int rdadata_sync_from(std::string remote_filename,std::string local_filename,std::string rdadata_home,std::stringstream& ess)
+{
+// special tokens in remote_filename
+//  __HOST__ will be replaced with hosts.getCurrent()
+//
+  std::list<std::string> hosts;
+  std::stringstream oss;
+  int status=0;
+  struct stat buf;
+
+  if (!unixutils::hosts_loaded(hosts,rdadata_home)) {
+    ess << "unable to get list of host names" << std::endl;
+    return -1;
+  }
+  for (auto& host : hosts) {
+    std::string rname=remote_filename;
+    strutils::replace_all(rname,"__HOST__",host);
+    if (mysystem2("/bin/sh -c \""+rdadata_home+"/bin/"+host+"-sync -g "+rname+" "+local_filename+"\"",oss,ess) == 0 && stat(local_filename.c_str(),&buf) == 0) {
+	break;
+    }
+    else {
+	status=-1;
+    }
+  }
+  return status;
+}
+
+bool exists_on_server(std::string host_name,std::string remote_filename,std::string rdadata_home)
+{
+  std::stringstream output,error;
+  mysystem2("/bin/sh -c 'rsync --list-only -e __INNER_QUOTE__ssh -i "+rdadata_home+"/.ssh/"+strutils::token(host_name,".",0)+"-sync_rdadata_rsa -l rdadata__INNER_QUOTE__ "+host_name+":"+remote_filename+"'",output,error);
+  if (!output.str().empty()) {
+    if (error.str().empty()) {
+	return true;
+    }
+    else {
+	return false;
+    }
+  }
+  else {
+    return false;
+  }
+}
+
+} // end namespace unixutils
