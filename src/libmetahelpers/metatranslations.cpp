84e83b92588730d6da0c4f88c8c7730413c61204
rda-dattore <dattore@ucar.edu>
2018-05-11 16:27:49 -0600

    updates

--- src/libmetahelpers/metatranslations.cpp
+++ src/libmetahelpers/metatranslations.cpp
@@ -9,31 +9,27 @@ namespace metatranslations {
 std::string detailed_datatype(xmlutils::DataTypeMapper& data_type_mapper,const std::string& data_format,const std::string& code)
 {
   auto idx=code.find(":");
-  auto detailedDataType=data_type_mapper.description(data_format,code.substr(0,idx),code.substr(idx+1));
-  if (detailedDataType.length() == 0) {
-    detailedDataType=data_format+":"+code;
+  auto detailed_datatype=data_type_mapper.description(data_format,code.substr(0,idx),code.substr(idx+1));
+  if (detailed_datatype.empty()) {
+    detailed_datatype=data_format+":"+code;
   }
   auto vars=data_type_mapper.variables(data_format,code.substr(0,idx),code.substr(idx+1));
   if (vars.size() > 0) {
-    detailedDataType+=" (may include one or more of the following)<ul>";
+    detailed_datatype+=" (may include one or more of the following)<ul>";
     for (auto& var : vars) {
-	detailedDataType+="<li>"+var+"</li>";
+	detailed_datatype+="<li>"+var+"</li>";
     }
-    detailedDataType+="</ul>";
+    detailed_datatype+="</ul>";
   }
-  return detailedDataType;
+  return detailed_datatype;
 }
 
 std::string detailed_level(xmlutils::LevelMapper& level_mapper,const std::string& data_format,const std::string& map,const std::string& type,const std::string& value,bool htmlify_units)
 {
-  std::string detailed_level;
+  auto detailed_level=level_mapper.description(data_format,type,map);
   std::string units_b,units_t;
-  std::deque<std::string> sp;
-  size_t idx;
-
-  detailed_level=level_mapper.description(data_format,type,map);
   if (strutils::contains(type,"-")) {
-    sp=strutils::split(type,"-");
+    auto sp=strutils::split(type,"-");
     units_b=level_mapper.units(data_format,sp[0],map);
     units_t=level_mapper.units(data_format,sp[1],map);
   }
@@ -41,20 +37,20 @@ std::string detailed_level(xmlutils::LevelMapper& level_mapper,const std::string
     units_b=level_mapper.units(data_format,type,map);
     units_t=units_b;
   }
-  if (units_b.length() > 0 && htmlify_units) {
+  if (!units_b.empty() && htmlify_units) {
     units_b=htmlutils::transform_superscripts(units_b);
   }
-  if (units_t.length() > 0 && htmlify_units) {
+  if (!units_t.empty() && htmlify_units) {
     units_t=htmlutils::transform_superscripts(units_t);
   }
   if (strutils::contains(value,",")) {
     if (value != "0,0") {
-	idx=value.find(",");
+	auto idx=value.find(",");
 	detailed_level+=": Bottom="+value.substr(0,idx)+" "+units_b+", Top="+value.substr(idx+1)+" "+units_t;
     }
   }
   else {
-    if (value != "0" || units_b.length() > 0) {
+    if (value != "0" || !units_b.empty()) {
 	detailed_level+=": "+value+" "+units_b;
     }
   }
