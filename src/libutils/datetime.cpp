2368e81b3b5d22208aa1ae4b2ef398ec77700e97
rda-dattore <dattore@ucar.edu>
2018-10-05 12:10:35 -0600

    moved in functions from libmetadata

--- src/libutils/datetime.cpp
+++ src/libutils/datetime.cpp
@@ -816,24 +816,6 @@ DateTime current_date_time()
   return dt;
 }
 
-bool is_leap_year(size_t year,std::string calendar)
-{
-  if (calendar.empty() || calendar == "standard" || calendar == "gregorian" || calendar == "proleptic_gregorian") {
-    if ( (year % 4) == 0 && ( (year % 100 != 0) || (year % 400) == 0)) {
-	return true;
-    }
-    else {
-	return false;
-    }
-  }
-  else if (calendar == "366_day" || calendar == "all_leap") {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
 size_t days_in_month(size_t year,size_t month,std::string calendar)
 {
   if (month < 1 || month > 12) {
@@ -850,28 +832,6 @@ size_t days_in_month(size_t year,size_t month,std::string calendar)
 	return DateTime::days_in_month_noleap[month];
     }
   }
-}
-
-int julian_day(size_t year,size_t month,size_t day,size_t base_year)
-{
-  size_t days_per_month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-  if (year < base_year) {
-    std::cout << "julian_day(): error -- " << year << " is less than base year " << base_year << std::endl;
-    return -1;
-  }
-  --month;
-// sum the days up to the current year
-  for (size_t n=base_year; n < year; ++n) {
-    day+= ( (n % 4) == 0) ? 366 : 365;
-  }
-// sum the days up to the current month
-  if ( (year % 4) == 0) {
-    days_per_month[1]=29;
-  }
-  for (size_t n=0; n < month; ++n) {
-    day+=days_per_month[n];
-  }
-  return day;
 }
 
 void decode_julian_day(int jul_day,size_t& year,size_t& month,size_t& day,size_t base_year)
@@ -904,6 +864,74 @@ void decode_julian_day(int jul_day,size_t& year,size_t& month,size_t& day,size_t
   month=n;
 // get the day for this julian day
   day=jul_day;
+}
+
+bool is_leap_year(size_t year,std::string calendar)
+{
+  if (calendar.empty() || calendar == "standard" || calendar == "gregorian" || calendar == "proleptic_gregorian") {
+    if ( (year % 4) == 0 && ( (year % 100 != 0) || (year % 400) == 0)) {
+	return true;
+    }
+    else {
+	return false;
+    }
+  }
+  else if (calendar == "366_day" || calendar == "all_leap") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+int julian_day(size_t year,size_t month,size_t day,size_t base_year)
+{
+  size_t days_per_month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+  if (year < base_year) {
+    std::cout << "julian_day(): error -- " << year << " is less than base year " << base_year << std::endl;
+    return -1;
+  }
+  --month;
+// sum the days up to the current year
+  for (size_t n=base_year; n < year; ++n) {
+    day+= ( (n % 4) == 0) ? 366 : 365;
+  }
+// sum the days up to the current month
+  if ( (year % 4) == 0) {
+    days_per_month[1]=29;
+  }
+  for (size_t n=0; n < month; ++n) {
+    day+=days_per_month[n];
+  }
+  return day;
+}
+
+std::string string_date_to_ll_string(std::string date)
+{
+  strutils::replace_all(date,"-","");
+  strutils::replace_all(date," ","");
+  strutils::replace_all(date,":","");
+  return date;
+}
+
+std::string string_ll_to_date_string(std::string ll_date)
+{
+  if (ll_date.length() > 4) {
+    ll_date.insert(4,"-");
+  }
+  if (ll_date.length() > 7) {
+    ll_date.insert(7,"-");
+  }
+  if (ll_date.length() > 10) {
+    ll_date.insert(10," ");
+    if (ll_date.length() > 13) {
+        ll_date.insert(13,":");
+        if (ll_date.length() > 16) {
+          ll_date.insert(16,":");
+        }
+    }
+  }
+  return ll_date;
 }
 
 } // end namespace dateutils
