2c4bb9a4c1777b9ce2d55e35c6f88b99db17576b
rda-dattore <dattore@ucar.edu>
2019-10-10 13:34:22 -0600

    set location of gaussian data for GRIB grids

--- src/libgrids/print_grids.cpp
+++ src/libgrids/print_grids.cpp
@@ -9,106 +9,89 @@ int print(std::string input_filename,size_t format,bool headers_only,bool verbos
   Grid *source_grid;
   GRIBMessage *msg=NULL;
   switch (format) {
-    case Grid::cgcm1Format:
-    {
+    case Grid::cgcm1Format: {
 	grid_stream=new InputCGCM1GridStream;
 	source_grid=new CGCM1Grid;
 	break;
     }
-    case Grid::dslpFormat:
-    {
+    case Grid::dslpFormat: {
 	grid_stream=new InputDiamondSLPGridStream;
 	source_grid=new DiamondSLPGrid;
 	break;
     }
-    case Grid::gribFormat:
-    {
+    case Grid::gribFormat: {
 	grid_stream=new InputGRIBStream;
 	msg=new GRIBMessage;
 	source_grid=new GRIBGrid;
 	break;
     }
-    case Grid::grib2Format:
-    {
+    case Grid::grib2Format: {
 	grid_stream=new InputGRIBStream;
 	msg=new GRIB2Message;
 	source_grid=new GRIB2Grid;
 	break;
     }
-    case Grid::jraieeemmFormat:
-    {
+    case Grid::jraieeemmFormat: {
 	grid_stream=new InputJRAIEEEMMGridStream;
 	source_grid=new JRAIEEEMMGrid;
 	break;
     }
-    case Grid::latlonFormat:
-    {
+    case Grid::latlonFormat: {
 	grid_stream=new InputLatLonGridStream;
 	source_grid=new LatLonGrid;
 	break;
     }
-    case Grid::latlon25Format:
-    {
+    case Grid::latlon25Format: {
 	grid_stream=new InputLatLonGridStream(2.5);
 	source_grid=new LatLonGrid(2.5);
 	break;
     }
-    case Grid::navyFormat:
-    {
+    case Grid::navyFormat: {
 	grid_stream=new InputNavyGridStream;
 	source_grid=new NavyGrid;
 	break;
     }
-    case Grid::octagonalFormat:
-    {
+    case Grid::octagonalFormat: {
 	grid_stream=new InputOctagonalGridStream;
 	source_grid=new OctagonalGrid;
 	break;
     }
-    case Grid::oldlatlonFormat:
-    {
+    case Grid::oldlatlonFormat: {
 	grid_stream=new InputOldLatLonGridStream;
 	source_grid=new OldLatLonGrid;
 	break;
     }
-    case Grid::on84Format:
-    {
+    case Grid::on84Format: {
 	grid_stream=new InputON84GridStream;
 	source_grid=new ON84Grid;
 	break;
     }
-    case Grid::slpFormat:
-    {
+    case Grid::slpFormat: {
 	grid_stream=new InputSLPGridStream;
 	source_grid=new SLPGrid;
 	break;
     }
-    case Grid::tdlgribFormat:
-    {
+    case Grid::tdlgribFormat: {
 	grid_stream=new InputTDLGRIBGridStream;
 	source_grid=new TDLGRIBGrid;
 	break;
     }
-    case Grid::tropicalFormat:
-    {
+    case Grid::tropicalFormat: {
 	grid_stream=new InputTropicalGridStream;
 	source_grid=new TropicalGrid;
 	break;
     }
-    case Grid::ukslpFormat:
-    {
+    case Grid::ukslpFormat: {
 	grid_stream=new InputUKSLPGridStream;
 	source_grid=new UKSLPGrid;
 	break;
     }
-    case Grid::ussrslpFormat:
-    {
+    case Grid::ussrslpFormat: {
 	grid_stream=new InputUSSRSLPGridStream;
 	source_grid=new USSRSLPGrid;
 	break;
     }
-    default:
-    {
+    default: {
 	std::cerr << "Error: invalid format specified " << format << std::endl;
 	return 1;
     }
@@ -143,6 +126,7 @@ int print(std::string input_filename,size_t format,bool headers_only,bool verbos
 	  if (format == Grid::gribFormat || format == Grid::grib2Format) {
 	    for (size_t n=0; n < msg->number_of_grids(); ++n) {
 		source_grid=msg->grid(n);
+		source_grid->set_path_to_gaussian_latitude_data("/glade/u/home/rdadata/share/GRIB");
 		source_grid->print(std::cout);
 	    }
 	  }
