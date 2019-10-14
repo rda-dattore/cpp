1a26af75ccf080965b1950ba5d47b53c81419bf7
rda-dattore <dattore@ucar.edu>
2019-10-14 13:43:38 -0600

    add PreparedStatement class

--- src/libmysql/MySQL.cpp
+++ src/libmysql/MySQL.cpp
@@ -264,7 +264,7 @@ int Server::insert(const std::string& absolute_table,const std::string& column_l
   mysql_real_query(&mysql,insert_string.c_str(),insert_string.length());
   _error="";
   if (mysql_errno(&mysql) > 0) {
-    _error=mysql_error(&mysql)+std::string(" - errno: ")+strutils::itos(mysql_errno(&mysql));
+    _error=mysql_error(&mysql)+std::string(" - errno: ")+strutils::itos(mysql_errno(&mysql))+"\nfailed insert: '"+insert_string+"'";
   }
   if (!_error.empty()) {
     return -1;
@@ -554,6 +554,232 @@ QueryIterator LocalQuery::begin()
 {
   rewind();
   return Query::begin();
+}
+
+bool PreparedStatement::bind_parameter(size_t parameter_number,float parameter_specification,bool is_null)
+{
+  if (parameter_number >= input_bind.lengths.size()) {
+    _error="parameter number out of range";
+    return false;
+  }
+  if (input_bind.binds[parameter_number].buffer_type != MYSQL_TYPE_FLOAT) {
+    _error="float bind does not match parameter specification";
+    return false;
+  }
+  input_bind.is_nulls[parameter_number]=is_null;
+  input_bind.binds[parameter_number].is_null=&input_bind.is_nulls[parameter_number];
+  if (is_null) {
+    if (input_bind.binds[parameter_number].buffer != nullptr) {
+	delete reinterpret_cast<float *>(input_bind.binds[parameter_number].buffer);
+    }
+    input_bind.binds[parameter_number].buffer=nullptr;
+    input_bind.binds[parameter_number].buffer_length=0;
+  }
+  else {
+    input_bind.binds[parameter_number].buffer=new float;
+    *(reinterpret_cast<float *>(input_bind.binds[parameter_number].buffer))=parameter_specification;
+    input_bind.binds[parameter_number].buffer_length=sizeof(float);
+  }
+  input_bind.lengths[parameter_number]=sizeof(float);
+  input_bind.binds[parameter_number].length=&input_bind.lengths[parameter_number];
+  return true;
+}
+
+bool PreparedStatement::bind_parameter(size_t parameter_number,int parameter_specification,bool is_null)
+{
+  if (parameter_number >= input_bind.lengths.size()) {
+    _error="parameter number out of range";
+    return false;
+  }
+  if (input_bind.binds[parameter_number].buffer_type != MYSQL_TYPE_LONG) {
+    _error="int bind does not match parameter specification";
+    return false;
+  }
+  input_bind.is_nulls[parameter_number]=is_null;
+  input_bind.binds[parameter_number].is_null=&input_bind.is_nulls[parameter_number];
+  if (is_null) {
+    if (input_bind.binds[parameter_number].buffer != nullptr) {
+	delete reinterpret_cast<int *>(input_bind.binds[parameter_number].buffer);
+    }
+    input_bind.binds[parameter_number].buffer=nullptr;
+    input_bind.binds[parameter_number].buffer_length=0;
+  }
+  else {
+    input_bind.binds[parameter_number].buffer=new int;
+    *(reinterpret_cast<int *>(input_bind.binds[parameter_number].buffer))=parameter_specification;
+    input_bind.binds[parameter_number].buffer_length=sizeof(int);
+  }
+  input_bind.lengths[parameter_number]=sizeof(int);
+  input_bind.binds[parameter_number].length=&input_bind.lengths[parameter_number];
+  return true;
+}
+
+bool PreparedStatement::bind_parameter(size_t parameter_number,std::string parameter_specification,bool is_null)
+{
+  if (parameter_number >= input_bind.lengths.size()) {
+    _error="parameter number out of range";
+    return false;
+  }
+  if (input_bind.binds[parameter_number].buffer_type != MYSQL_TYPE_STRING) {
+    _error="string bind does not match parameter specification";
+    return false;
+  }
+  input_bind.is_nulls[parameter_number]=is_null;
+  input_bind.binds[parameter_number].is_null=&input_bind.is_nulls[parameter_number];
+  if (is_null) {
+    if (input_bind.binds[parameter_number].buffer != nullptr) {
+	delete[] reinterpret_cast<char *>(input_bind.binds[parameter_number].buffer);
+    }
+    auto len=0;
+    input_bind.lengths[parameter_number]=len;
+    input_bind.binds[parameter_number].buffer=nullptr;
+    input_bind.binds[parameter_number].buffer_length=len;
+  }
+  else {
+    auto len=parameter_specification.length();
+    input_bind.lengths[parameter_number]=len;
+    input_bind.binds[parameter_number].buffer=new char[len+1];
+    std::copy(parameter_specification.begin(),parameter_specification.end(),reinterpret_cast<char *>(input_bind.binds[parameter_number].buffer));
+    reinterpret_cast<char *>(input_bind.binds[parameter_number].buffer)[len]='\0';
+    input_bind.binds[parameter_number].buffer_length=len;
+  }
+  input_bind.binds[parameter_number].length=&input_bind.lengths[parameter_number];
+  return true;
+}
+
+bool PreparedStatement::bind_parameter(size_t parameter_number,const char *parameter_specification,bool is_null)
+{
+  return bind_parameter(parameter_number,std::string(parameter_specification),is_null);
+}
+
+size_t PreparedStatement::num_rows() const
+{
+  if (STMT == nullptr) {
+    return 0;
+  }
+  else {
+    return mysql_stmt_num_rows(STMT.get());
+  }
+}
+
+bool PreparedStatement::fetch_row(Row& row) const
+{
+  if (mysql_stmt_fetch(STMT.get()) == 0) {
+    std::stringstream row_ss;
+    for (size_t n=0; n < result_bind.field_count; ++n) {
+	if (!row_ss.str().empty()) {
+	  row_ss << "<!>";
+	}
+	row_ss << std::string(reinterpret_cast<char *>(result_bind.binds[n].buffer),*result_bind.binds[n].length);
+    }
+    row.fill(row_ss.str(),"<!>",const_cast<std::unordered_map<std::string,size_t>*>(&column_list));
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void PreparedStatement::set(std::string statement_specification,std::vector<enum_field_types> parameter_types)
+{
+  statement=statement_specification;
+  input_bind.binds.reset(new MYSQL_BIND[parameter_types.size()]);
+  input_bind.lengths.resize(parameter_types.size(),0);
+  input_bind.is_nulls.resize(parameter_types.size(),0);
+  for (size_t n=0; n < parameter_types.size(); ++n) {
+    input_bind.binds[n].buffer_type=parameter_types[n];
+    input_bind.binds[n].buffer=nullptr;
+  }
+  STMT.reset(nullptr);
+  column_list.clear();
+}
+
+int PreparedStatement::submit(Server& server)
+{
+  if (STMT == nullptr) {
+    STMT.reset(mysql_stmt_init(server.handle()));
+    if (STMT == nullptr) {
+	_error="unable to initialize statement";
+	return -1;
+    }
+  }
+  if (mysql_stmt_prepare(STMT.get(),statement.c_str(),statement.length()) != 0) {
+    _error="unable to prepare statement - "+std::string(mysql_stmt_error(STMT.get()));
+    return -1;
+  }
+  if (mysql_stmt_param_count(STMT.get()) != input_bind.lengths.size()) {
+    _error="bad parameter count - "+strutils::itos(mysql_stmt_param_count(STMT.get()))+"/"+strutils::itos(input_bind.lengths.size())+"\nstatement: "+statement;
+    return -1;
+  }
+  if (mysql_stmt_bind_param(STMT.get(),input_bind.binds.get()) != 0) {
+    _error="unable to bind parameter(s)";
+    exit(1);
+  }
+  if (mysql_stmt_execute(STMT.get()) != 0) {
+    _error="unable to execute statement";
+    exit(1);
+  }
+  result_bind.field_count=mysql_stmt_field_count(STMT.get());
+  if (result_bind.field_count > 0) {
+    result_bind.binds.reset(new MYSQL_BIND[result_bind.field_count]);
+    result_bind.lengths.resize(result_bind.field_count);
+    result_bind.is_nulls.resize(result_bind.field_count);
+    result_bind.errors.resize(result_bind.field_count);
+    std::vector<char *> fields;
+    for (size_t n=0; n < result_bind.field_count; ++n) {
+	result_bind.binds[n].buffer_type=MYSQL_TYPE_STRING;
+	result_bind.binds[n].buffer_length=255;
+	fields.emplace_back(new char[result_bind.binds[n].buffer_length]);
+	result_bind.binds[n].buffer=fields[n];
+	result_bind.binds[n].length=&result_bind.lengths[n];
+	result_bind.binds[n].is_null=&result_bind.is_nulls[n];
+	result_bind.binds[n].error=&result_bind.errors[n];
+    }
+    if (mysql_stmt_bind_result(STMT.get(),result_bind.binds.get()) != 0) {
+	_error="unable to bind result set";
+	return -1;
+    }
+    if (mysql_stmt_store_result(STMT.get()) != 0) {
+	_error="unable to store the result set";
+	return -1;
+    }
+    if (column_list.size() == 0) {
+	auto field_metadata=mysql_stmt_result_metadata(STMT.get());
+	auto columns=mysql_fetch_fields(field_metadata);
+	for (size_t n=0; n < mysql_num_fields(field_metadata); ++n) {
+	  column_list[columns[n].name]=n;
+	}
+    }
+  }
+  return 0;
+}
+
+PreparedStatementIterator PreparedStatement::begin()
+{
+  auto i=PreparedStatementIterator(*this,false);
+  ++i;
+  return i;
+}
+
+PreparedStatementIterator PreparedStatement::end()
+{
+  return PreparedStatementIterator(*this,true);
+}
+
+const PreparedStatementIterator& PreparedStatementIterator::operator++()
+{
+  if (!_pstmt.fetch_row(row)) {
+    _is_end=true;
+  }
+  return *this;
+}
+
+Row& PreparedStatementIterator::operator*()
+{
+  if (_is_end) {
+    row=Row();
+  }
+  return row;
 }
 
 bool table_exists(Server& server,std::string absolute_table)
