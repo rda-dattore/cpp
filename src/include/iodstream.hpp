c71928b2fc1906960e232ea8a9ff19d1337056d6
rda-dattore <dattore@ucar.edu>
2021-06-07 08:23:30 -0600

    restyled

--- src/include/iodstream.hpp
+++ src/include/iodstream.hpp
@@ -6,15 +6,12 @@
 #include <iostream>
 #include <bfstream.hpp>
 
-class iods
-{
+class iods {
 public:
-  enum {
-    binary,cos,rptout,cos_rptout
-  };
-  iods() : file_name(),fs() {}
-  virtual ~iods() {}
-  virtual void close()=0;
+  enum { binary, cos, rptout, cos_rptout };
+  iods() : file_name(), fs() { }
+  virtual ~iods() { }
+  virtual void close() = 0;
   std::string name() const { return file_name; }
   virtual bool is_open() const { return (fs.is_open()); }
 
@@ -24,41 +21,45 @@ protected:
 };
 
 class idstream;
-class idstream_iterator
-{
+class idstream_iterator {
 public:
-  idstream_iterator(idstream& stream,bool is_end) : _stream(stream),_is_end(is_end),buffer(nullptr),buffer_capacity(0) {}
-  bool operator !=(const idstream_iterator& source) { return (_is_end != source._is_end); }
+  idstream_iterator(idstream& stream, bool is_end) : stream(stream), is_end(
+      is_end), buffer(nullptr), buffer_capacity(0) { }
+  bool operator !=(const idstream_iterator& source) { return is_end !=
+      source.is_end; }
   const idstream_iterator& operator++();
   unsigned char *operator*();
 
 private:
-  idstream &_stream;
-  bool _is_end;
+  idstream& stream;
+  bool is_end;
   std::unique_ptr<unsigned char[]> buffer;
   int buffer_capacity;
 };
 
-class idstream : public iods
-{
+class idstream : public iods {
 public:
-  idstream() : icosstream(nullptr),irptstream(nullptr),if77_stream(nullptr),ivstream(nullptr),num_read(0) {}
+  idstream() : icosstream(nullptr), irptstream(nullptr), if77_stream(nullptr),
+      ivstream(nullptr), num_read(0) { }
 
 // pure virtual functions from iods
   virtual ~idstream() { close(); }
-  virtual bool is_open() const { return (fs.is_open() || icosstream != nullptr || irptstream != nullptr || if77_stream != nullptr || ivstream != nullptr); }
+  virtual bool is_open() const {
+      return (fs.is_open() || icosstream != nullptr || irptstream != nullptr ||
+          if77_stream != nullptr || ivstream != nullptr);
+  }
 
 // pure virtual functions making idstream an abstract class
-  virtual int ignore()=0;
-  virtual int peek()=0;
-  virtual int read(unsigned char *buffer,size_t buffer_length)=0;
+  virtual int ignore() = 0;
+  virtual int peek() = 0;
+  virtual int read(unsigned char *buffer, size_t buffer_length) = 0;
 
 // local methods
   virtual void close();
   virtual off_t current_record_offset() const { return -1; }
   size_t number_read() const { return num_read; }
   virtual bool open(std::string filename);
-  bool is_cos_blocked() const { return (icosstream != nullptr); }
+  bool is_cos_blocked() const { return icosstream != nullptr; }
   virtual void rewind();
 
 // range-base support
@@ -73,17 +74,19 @@ protected:
   size_t num_read;
 };
 
-class odstream : public iods
-{
+class odstream : public iods {
 public:
-  odstream() : ocosstream(nullptr),orptstream(nullptr),ocrptstream(nullptr),num_written(0) {}
+  odstream() : ocosstream(nullptr), orptstream(nullptr), ocrptstream(nullptr),
+      num_written(0) { }
   virtual ~odstream() { close(); }
   virtual void close();
-  virtual bool is_open() const { return (fs.is_open() || ocosstream != nullptr || orptstream != nullptr || ocrptstream != nullptr);
+  virtual bool is_open() const {
+      return (fs.is_open() || ocosstream != nullptr || orptstream != nullptr ||
+          ocrptstream != nullptr);
   }
   size_t number_written() const { return num_written; }
-  virtual bool open(std::string filename,size_t blocking_flag=binary);
-  virtual int write(const unsigned char *buffer,size_t num_bytes);
+  virtual bool open(std::string filename, size_t blocking_flag = binary);
+  virtual int write(const unsigned char *buffer, size_t num_bytes);
 
 protected:
   std::unique_ptr<ocstream> ocosstream;
