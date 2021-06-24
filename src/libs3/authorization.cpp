a951d1d26edd7f32ec38711eb894bfc2b54d57ec
rda-dattore <dattore@ucar.edu>
2021-06-24 06:57:42 -0600

    use myssl namespace

--- src/libs3/authorization.cpp
+++ src/libs3/authorization.cpp
@@ -8,16 +8,16 @@ namespace s3 {
 unsigned char *signature(unsigned char *signing_key,const CanonicalRequest& canonical_request,std::string region,std::string terminal,unsigned char (&message_digest)[32])
 {
   std::string string_to_sign="AWS4-HMAC-SHA256\n"+canonical_request.timestamp()+"\n"+canonical_request.date()+"/"+region+"/s3/"+terminal+"\n"+canonical_request.hashed();
-  return hmac_sha256(signing_key,32,string_to_sign,message_digest);
+  return myssl::hmac_sha256(signing_key,32,string_to_sign,message_digest);
 }
 
 unsigned char *signing_key(std::string secret_key,std::string date,std::string region,std::string terminal,unsigned char (&message_digest)[32])
 {
   unsigned char md[3][32];
-  hmac_sha256("AWS4"+secret_key,date,md[0]);
-  hmac_sha256(md[0],32,region,md[1]);
-  hmac_sha256(md[1],32,"s3",md[2]);
-  return hmac_sha256(md[2],32,terminal,message_digest);
+  myssl::hmac_sha256("AWS4"+secret_key,date,md[0]);
+  myssl::hmac_sha256(md[0],32,region,md[1]);
+  myssl::hmac_sha256(md[1],32,"s3",md[2]);
+  return myssl::hmac_sha256(md[2],32,terminal,message_digest);
 }
 
 } // end namespace s3
