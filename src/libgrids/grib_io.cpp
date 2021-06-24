579571457c55067960c12cfb7182a679558ae707
rda-dattore <dattore@ucar.edu>
2021-06-24 13:51:27 -0600

    initial commit

---
+++ src/libgrids/grib_io.cpp
@@ -0,0 +1,327 @@
+#include <grid.hpp>
+#include <bits.hpp>
+#include <myexception.hpp>
+
+using std::runtime_error;
+using std::string;
+
+int InputGRIBStream::peek() {
+  unsigned char buffer[16];
+  int len = -9;
+  while (len == -9) {
+    len = find_grib(buffer);
+    if (len < 0) {
+      return len;
+    }
+    if (is3s != nullptr) {
+      auto n = is3s->read(&buffer[4], 12);
+      if (n > 0) {
+        len += n;
+      }
+    }
+    else {
+      fs.read(reinterpret_cast<char *>(&buffer[4]), 12);
+      len += fs.gcount();
+    }
+    if (len != 16) {
+      return bfstream::error;
+    }
+    switch (static_cast<int>(buffer[7])) {
+      case 0: {
+        len = 4;
+        fs.seekg(curr_offset + 4, std::ios_base::beg);
+        fs.read(reinterpret_cast<char *>(buffer), 3);
+        if (fs.eof()) {
+          return bfstream::eof;
+        } else if (!fs.good()) {
+          return bfstream::error;
+        }
+        while (buffer[0] != '7' && buffer[1] != '7' && buffer[2] != '7') {
+          int llen;
+          bits::get(buffer, llen, 0, 24);
+          if (llen <= 0) {
+            return bfstream::error;
+          }
+          len += llen;
+          fs.seekg(llen - 3, std::ios_base::cur);
+          fs.read(reinterpret_cast<char *>(buffer), 3);
+          if (fs.eof()) {
+            return bfstream::eof;
+          } else if (!fs.good()) {
+            return bfstream::error;
+          }
+        }
+        len += 4;
+        break;
+      }
+      case 1: {
+        bits::get(buffer, len, 32, 24);
+
+        // check for ECMWF large-file
+        if (len >= 0x800000) {
+          int computed_len = 0;
+          int sec_len, flag;
+
+          // PDS length
+          bits::get(buffer, sec_len, 64, 24);
+          bits::get(buffer, flag, 120, 8);
+          computed_len = 8 + sec_len;
+          fs.seekg(curr_offset + computed_len, std::ios_base::beg);
+
+          // GDS included?
+          if ( (flag & 0x80) == 0x80) {
+            fs.read(reinterpret_cast<char *>(buffer), 3);
+            bits::get(buffer, sec_len, 0, 24);
+            fs.seekg(sec_len - 3, std::ios_base::cur);
+            computed_len += sec_len;
+          }
+
+          // BMS included?
+          if ( (flag & 0x40) == 0x40) {
+            fs.read(reinterpret_cast<char *>(buffer), 3);
+            bits::get(buffer, sec_len, 0, 24);
+            fs.seekg(sec_len - 3, std::ios_base::cur);
+            computed_len += sec_len;
+          }
+          fs.read(reinterpret_cast<char *>(buffer), 3);
+
+          // BDS length
+          bits::get(buffer, sec_len, 0, 24);
+          if (sec_len < 120) {
+            len &= 0x7fffff;
+            len *= 120;
+            len -= sec_len;
+            fs.seekg(curr_offset + len, std::ios_base::beg);
+            len += 4;
+          } else {
+            fs.seekg(sec_len - 3, std::ios_base::cur);
+          }
+        } else {
+          if (is3s != nullptr) {
+            is3s->seek(is3s->tell() + len - 20);
+          } else {
+            fs.seekg(len - 20, std::ios_base::cur);
+          }
+        }
+        if (is3s != nullptr) {
+          is3s->read(buffer, 4);
+        } else {
+          fs.read(reinterpret_cast<char *>(buffer), 4);
+        }
+        if (string(reinterpret_cast<char *>(buffer), 4) != "7777") {
+          len =- 9;
+          if (is3s != nullptr) {
+            is3s->seek(curr_offset + 4);
+          } else {
+            fs.seekg(curr_offset + 4, std::ios_base::beg);
+          }
+        }
+        break;
+      }
+      case 2: {
+        bits::get(buffer, len, 96, 32);
+        break;
+      }
+      default: {
+        return peek();
+      }
+    }
+  }
+  if (is3s != nullptr) {
+    is3s->seek(curr_offset);
+  } else {
+    fs.seekg(curr_offset, std::ios_base::beg);
+  }
+  return len;
+}
+
+int InputGRIBStream::read(unsigned char *buffer, size_t buffer_length) {
+std::unique_ptr<unsigned char[]> b(buffer);
+buffer = b.release();
+  int bytes_read = 0;
+  int len = -9;
+  while (len == -9) {
+    bytes_read = find_grib(buffer);
+    if (bytes_read < 0) {
+      return bytes_read;
+    }
+    fs.read(reinterpret_cast<char *>(&buffer[4]), 12);
+    bytes_read += fs.gcount();
+    if (bytes_read != 16) {
+      if (fs.eof()) {
+        return bfstream::eof;
+      } else {
+        return bfstream::error;
+      }
+    }
+    switch (static_cast<int>(buffer[7])) {
+      case 0: {
+        fs.read(reinterpret_cast<char *>(&buffer[16]), 15);
+        bytes_read += fs.gcount();
+        if (bytes_read != 31) {
+          return bfstream::eof;
+        }
+        len = 28;
+        while (buffer[bytes_read-3] != '7' && buffer[bytes_read-2] != '7' &&
+            buffer[bytes_read-1] != '7') {
+          int llen;
+          bits::get(buffer, llen, len * 8, 24);
+          if (llen <= 0) {
+            return bfstream::error;
+          }
+          len += llen;
+          if (len > static_cast<int>(buffer_length)) {
+            throw my::BufferOverflow_Error("input buffer too small.");
+          }
+          fs.read(reinterpret_cast<char *>(&buffer[bytes_read]), llen);
+          bytes_read += fs.gcount();
+          if (bytes_read != len+3) {
+            if (fs.eof()) {
+              return bfstream::eof;
+            } else {
+              return bfstream::error;
+            }
+          }
+        }
+        fs.read(reinterpret_cast<char *>(&buffer[bytes_read]), 1);
+        bytes_read += fs.gcount();
+        break;
+      }
+      case 1: {
+        bits::get(buffer, len, 32, 24);
+        if (len > static_cast<int>(buffer_length)) {
+          throw my::BufferOverflow_Error("input buffer too small.");
+        }
+        fs.read(reinterpret_cast<char *>(&buffer[16]), len - 16);
+        bytes_read += fs.gcount();
+        if (bytes_read != len) {
+          if (fs.eof()) {
+            return bfstream::eof;
+          } else {
+            return bfstream::error;
+          }
+        }
+
+        // check for ECMWF large-file
+        if (len >= 0x800000) {
+          int computed_len=0, sec_len, flag;
+
+          // PDS length
+          bits::get(buffer, sec_len, 64, 24);
+          bits::get(buffer, flag, 120, 8);
+          computed_len = 8 + sec_len;
+
+          // GDS included
+          if ( (flag & 0x80) == 0x80) {
+            bits::get(buffer, sec_len, computed_len * 8, 24);
+            computed_len += sec_len;
+          }
+
+          // BMS included
+          if ( (flag & 0x40) == 0x40) {
+            bits::get(buffer, sec_len, computed_len * 8, 24);
+            computed_len += sec_len;
+          }
+
+          // BDS length
+          bits::get(buffer, sec_len, computed_len * 8, 24);
+          if (sec_len < 120) {
+            len &= 0x7fffff;
+            len *= 120;
+            len -= sec_len;
+            len += 4;
+            if (len > static_cast<int>(buffer_length)) {
+              throw my::BufferOverflow_Error("input buffer too small.");
+            }
+            fs.read(reinterpret_cast<char *>(&buffer[bytes_read]), len -
+                bytes_read);
+            bytes_read += fs.gcount();
+          }
+        }
+        if (string(reinterpret_cast<char *>(&buffer[len-4]), 4) !=
+            "7777") {
+          len =- 9;
+          fs.seekg(curr_offset + 4, std::ios_base::beg);
+        }
+        break;
+      }
+      case 2: {
+        bits::get(buffer, len, 96, 32);
+        if (len > static_cast<int>(buffer_length)) {
+          throw my::BufferOverflow_Error("input buffer too small.");
+        }
+        fs.read(reinterpret_cast<char *>(&buffer[16]), len - 16);
+        bytes_read += fs.gcount();
+        if (bytes_read != len) {
+          if (fs.eof()) {
+            return bfstream::eof;
+          } else {
+            return bfstream::error;
+          }
+        }
+        if (string(reinterpret_cast<char *>(buffer) + len - 4, 4) != "7777") {
+          throw my::NotFound_Error("unable to find GRIB END section.");
+        }
+        break;
+      }
+      default: {
+        return read(buffer, buffer_length);
+      }
+    }
+  }
+  ++num_read;
+  return bytes_read;
+}
+
+bool InputGRIBStream::open(string filename) {
+  idstream::open(filename);
+  if (is_open()) {
+    if (ics != nullptr) {
+      throw my::BadType_Error("COS-blocking must be removed from GRIB files.");
+    }
+    return true;
+  }
+  return false;
+}
+
+int InputGRIBStream::find_grib(unsigned char *buffer) {
+  auto b = reinterpret_cast<char *>(buffer);
+  b[0] = 'X';
+  int stat = 0;
+  if (is3s != nullptr) {
+    stat = is3s->read(buffer, 4);
+  } else {
+    fs.read(b, 4);
+  }
+  while (b[0] != 'G' || b[1] != 'R' || b[2] != 'I' || b[3] != 'B') {
+    if (is3s != nullptr) {
+      if (stat == bfstream::eof || stat == bfstream::error) {
+        if (num_read == 0) {
+          throw my::BadType_Error("file is not a GRIB file.");
+        }
+        return stat;
+      }
+    } else {
+      if (fs.eof() || !fs.good()) {
+        if (num_read == 0) {
+          throw my::BadType_Error("file is not a GRIB file.");
+        }
+        return (fs.eof()) ? bfstream::eof : bfstream::error;
+      }
+    }
+    b[0] = b[1];
+    b[1] = b[2];
+    b[2] = b[3];
+    if (is3s != nullptr) {
+      is3s->read(&buffer[3], 1);
+    } else {
+      fs.read(&b[3], 1);
+    }
+  }
+  if (is3s != nullptr) {
+    curr_offset = is3s->tell() - 4;
+  } else {
+    curr_offset = static_cast<off_t>(fs.tellg()) - 4;
+  }
+  return 4;
+}
