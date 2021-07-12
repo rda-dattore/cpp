2b46a486483310131e859eff68e91f59f62897c3
rda-dattore <dattore@ucar.edu>
2021-07-12 15:56:33 -0600

    restyled

--- src/libutilsthread/mysystem.cpp
+++ src/libutilsthread/mysystem.cpp
@@ -7,37 +7,42 @@
 #include <strutils.hpp>
 #include <utils.hpp>
 
+using std::move;
+using std::regex;
+using std::regex_search;
+using std::string;
+using std::stringstream;
+using strutils::chop;
+using strutils::replace_all;
+using strutils::split;
+
 namespace mysystem_t {
 
 struct ThreadStruct {
-  ThreadStruct() : tid(0),pipe(0),ss(nullptr) {}
+  ThreadStruct() : tid(0), pipe(0), ss(nullptr) { }
 
   pthread_t tid;
   int pipe;
-  std::stringstream *ss;
+  stringstream *ss;
 };
 
-extern "C" void *read_output(void *ts)
-{
-  ThreadStruct *t=(ThreadStruct *)ts;
-  int len;
+extern "C" void *read_output(void *ts) {
   char buffer[32768];
-
-  while ( (len=read(t->pipe,buffer,32768)) > 0) {
-    *(t->ss) << std::string(buffer,len);
+  auto *t = (ThreadStruct *)ts;
+  int len;
+  while ( (len = read(t->pipe, buffer, 32768)) > 0) {
+    *(t->ss) << string(buffer, len);
   }
   close(t->pipe);
   return nullptr;
 }
 
-extern "C" void *read_error(void *ts)
-{
-  ThreadStruct *t=(ThreadStruct *)ts;
-  int len;
+extern "C" void *read_error(void *ts) {
   char buffer[32768];
-
-  while ( (len=read(t->pipe,buffer,32768)) > 0) {
-    *(t->ss) << std::string(buffer,len);
+  auto *t = (ThreadStruct *)ts;
+  int len;
+  while ( (len = read(t->pipe, buffer, 32768)) > 0) {
+    *(t->ss) << string(buffer, len);
   }
   close(t->pipe);
   return nullptr;
@@ -47,134 +52,133 @@ extern "C" void *read_error(void *ts)
 
 namespace unixutils {
 
-std::string retry_command(std::string command,int num_retries)
-{
-  std::stringstream output,error;
-  std::string error_list;
-  int nr=0;
+string retry_command(string command, int num_retries) {
+  string s; // return value
+  int n = 0;
+  stringstream oss, ess;
+  if (mysystem2(command, oss, ess) != 0) {
 
-  if (mysystem2(command,output,error) != 0) {
-// deal with some specific situations
-    if (std::regex_search(error.str(),std::regex("^mv: preserving permissions"))) {
-	auto  sp=strutils::split(command);
-	if (sp[0] == "/bin/mv" && std::regex_search(sp[1],std::regex("^/tmp"))) {
-	  error.str("");
-	}
+    // deal with some specific situations
+    if (regex_search(ess.str(), regex("^mv: preserving permissions"))) {
+      auto sp = split(command);
+      if (sp[0] == "/bin/mv" && regex_search(sp[1], regex("^/tmp"))) {
+        ess.str("");
+      }
     }
-    while (!error.str().empty() && nr < num_retries) {
-	if (!error_list.empty()) {
-	  error_list+=";";
-	}
-	error_list+="'"+error.str()+"'";
-	std::this_thread::sleep_for(std::chrono::seconds(5));
-	++nr;
-	mysystem2(command,output,error);
+    while (!ess.str().empty() && n < num_retries) {
+      if (!s.empty()) {
+        s += ";";
+      }
+      s += "'" + ess.str() + "'";
+      std::this_thread::sleep_for(std::chrono::seconds(5));
+      ++n;
+      mysystem2(command, oss, ess);
     }
   }
-  if (nr < num_retries) {
-    error_list="";
+  if (n < num_retries) {
+    s = "";
   }
-  return error_list;
+  return move(s);
 }
 
-int mysystem2(std::string command,std::stringstream& output,std::stringstream& error)
-{
+int mysystem2(string command, stringstream& output, stringstream& error) {
   output.str("");
   error.str("");
-  int pipe_out[2],pipe_err[2];
-  pipe(pipe_out);
-  pipe(pipe_err);
-  auto child_pid=fork();
-  if (child_pid < 0) {
-    error << "Unable to fork";
+  int pipe_out[2], pipe_err[2];
+  if (pipe(pipe_out) != 0) {
+    error << "can't create output pipe";
     return -1;
   }
-  else if (child_pid == 0) {
-    std::string new_command;
-    char c_quote='\0';
-    auto in_quotes=false;
-    auto sp=strutils::split(command);
-    for (size_t n=0; n < sp.size(); ++n) {
-	if ((sp[n][0] == '"' || sp[n][0] == '\'') && !in_quotes) {
-	    c_quote=sp[n][0];
-	    sp[n]=sp[n].substr(1);
-	    in_quotes=true;
-	}
-	if (in_quotes && sp[n].back() == c_quote) {
-	  in_quotes=false;
-	  strutils::chop(sp[n]);
-	  c_quote='\0';
-	}
-	if (in_quotes) {
-	  new_command+=sp[n]+" ";
-	}
-	else {
-	  new_command+=sp[n]+"<!>";
-	}
+  if (pipe(pipe_err) != 0) {
+    error << "can't create error pipe";
+    return -1;
+  }
+  auto c = fork();
+  if (c < 0) {
+    error << "unable to fork";
+    return -1;
+  } else if (c == 0) {
+    string s;
+    char c_quote = '\0';
+    auto in_quotes = false;
+    auto sp = split(command);
+    for (size_t n = 0; n < sp.size(); ++n) {
+      if ((sp[n][0] == '"' || sp[n][0] == '\'') && !in_quotes) {
+          c_quote = sp[n][0];
+          sp[n] = sp[n].substr(1);
+          in_quotes = true;
+      }
+      if (in_quotes && sp[n].back() == c_quote) {
+        in_quotes = false;
+        chop(sp[n]);
+        c_quote = '\0';
+      }
+      if (in_quotes) {
+        s += sp[n] + " ";
+      }
+      else {
+        s += sp[n] + "<!>";
+      }
     }
-    strutils::chop(new_command,3);
-    sp=strutils::split(new_command,"<!>");
-    auto argv=new char *[sp.size()+1];
-    auto idx=sp[0].rfind("/");
-    if (idx == std::string::npos) {
-	argv[0]=new char[sp[0].length()+1];
-	strcpy(argv[0],sp[0].c_str());
+    chop(s, 3);
+    sp = split(s, "<!>");
+    auto argv = new char *[sp.size() + 1];
+    auto idx = sp[0].rfind("/");
+    if (idx == string::npos) {
+      argv[0] = new char[sp[0].length() + 1];
+      strcpy(argv[0], sp[0].c_str());
+    } else {
+      auto a = sp[0].substr(idx + 1);
+      argv[0] = new char[a.length() + 1];
+      strcpy(argv[0], a.c_str());
     }
-    else {
-	auto arg0=sp[0].substr(idx+1);
-	argv[0]=new char[arg0.length()+1];
-	strcpy(argv[0],arg0.c_str());
+    size_t last = 1;
+    for (size_t n = last; n < sp.size(); ++n) {
+      replace_all(sp[n], "__INNER_QUOTE__", "'");
+      if (sp[n] == "<") {
+        if (n == (sp.size() - 2)) {
+          auto d = open(sp[n + 1].c_str(), O_RDONLY);
+          dup2(d, 0);
+          close(d);
+          break;
+        } else {
+          error << "bad input redirection";
+          return -1;
+        }
+      } else {
+        argv[n] = new char[sp[n].length() + 1];
+        strcpy(argv[n], sp[n].c_str());
+      }
+      ++last;
     }
-    size_t last=1;
-    for (size_t n=last; n < sp.size(); ++n) {
-	strutils::replace_all(sp[n],"__INNER_QUOTE__","'");
-	if (sp[n] == "<") {
-	  if (n == (sp.size()-2)) {
-	    auto rstdin=open(sp[n+1].c_str(),O_RDONLY);
-	    dup2(rstdin,0);
-	    close(rstdin);
-	    break;
-	  }
-	  else {
-	    error << "bad input redirection";
-	    return -1;
-	  }
-	}
-	else {
-	  argv[n]=new char[sp[n].length()+1];
-	  strcpy(argv[n],sp[n].c_str());
-	}
-	++last;
-    }
-    argv[last]=nullptr;
+    argv[last] = nullptr;
     close(pipe_out[0]);
     close(pipe_err[0]);
-    dup2(pipe_out[1],1);
-    dup2(pipe_err[1],2);
-    execv(sp[0].c_str(),argv);
+    dup2(pipe_out[1], 1);
+    dup2(pipe_err[1], 2);
+    execv(sp[0].c_str(), argv);
     std::cerr << sp[0] << ": command not found" << std::endl;
     exit(1);
-  }
-  else {
+  } else {
     close(pipe_out[1]);
     close(pipe_err[1]);
-    mysystem_t::ThreadStruct tstructs[2];
-    tstructs[0].pipe=pipe_out[0];
-    tstructs[0].ss=&output;
-    pthread_create(&tstructs[0].tid,nullptr,mysystem_t::read_output,reinterpret_cast<void *>(&tstructs[0]));
-    tstructs[1].pipe=pipe_err[0];
-    tstructs[1].ss=&error;
-    pthread_create(&tstructs[1].tid,nullptr,mysystem_t::read_error,reinterpret_cast<void *>(&tstructs[1]));
-    pthread_join(tstructs[0].tid,nullptr);
-    pthread_join(tstructs[1].tid,nullptr);
-    int status;
-    auto pid=waitpid(child_pid,&status,0);
+    mysystem_t::ThreadStruct t[2];
+    t[0].pipe = pipe_out[0];
+    t[0].ss = &output;
+    pthread_create(&t[0].tid, nullptr, mysystem_t::read_output,
+        reinterpret_cast<void *>(&t[0]));
+    t[1].pipe = pipe_err[0];
+    t[1].ss = &error;
+    pthread_create(&t[1].tid, nullptr, mysystem_t::read_error, reinterpret_cast<
+        void *>(&t[1]));
+    pthread_join(t[0].tid, nullptr);
+    pthread_join(t[1].tid, nullptr);
+    int s;
+    auto pid = waitpid(c, &s, 0);
     if (pid < 0 || !error.str().empty()) {
-	return WEXITSTATUS(status);
+      return WEXITSTATUS(s);
     }
-    else {
-	return WEXITSTATUS(status);
-    }
+    return WEXITSTATUS(s);
   }
 }
 
