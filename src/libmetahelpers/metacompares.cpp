78892b51b0bb1670951349cf0bc1b84761e6a31b
rda-dattore <dattore@ucar.edu>
2021-11-11 10:49:15 -0700

    refactor and make returns adhere to strict weak ordering

--- src/libmetahelpers/metacompares.cpp
+++ src/libmetahelpers/metacompares.cpp
@@ -1,275 +1,207 @@
 #include <regex>
 #include <strutils.hpp>
 
+using std::regex;
+using std::regex_search;
+using std::stof;
+using std::stoi;
+using std::string;
+using std::vector;
+using strutils::chop;
+using strutils::split;
+using strutils::to_lower;
+using strutils::trim;
+
 namespace metacompares {
 
-bool compare_grid_definitions(const std::string& left,const std::string& right)
-{
-  if (left == right) {
+bool compare_grid_definitions(const string& left, const string& right) {
+  auto lf = left.find("&deg;");
+  auto rf = right.find("&deg;");
+  if (lf != string::npos && rf != string::npos) {
+    auto l = stof(left.substr(0, lf));
+    auto r = stof(right.substr(0, rf));
+    return l < r;
+  }
+  if (lf != string::npos) {
     return true;
   }
-  else {
-    auto lidx=left.find("&deg;");
-    auto ridx=right.find("&deg;");
-    if (lidx != std::string::npos && ridx != std::string::npos) {
-	auto l=std::stof(left.substr(0,lidx));
-	auto r=std::stof(right.substr(0,ridx));
-	if (l <= r) {
-	  return true;
-	}
-	else {
-	  return false;
-	}
-    }
-    else {
-	if (lidx != std::string::npos) {
-	  return true;
-	}
-	else if (ridx != std::string::npos) {
-	  return false;
-	}
-	else {
-	  if (left <= right) {
-	    return true;
-	  }
-	  else {
-	    return false;
-	  }
-	}
-    }
-  }
-}
-
-bool compare_levels(const std::string& left,const std::string& right)
-{
-  auto lval=left;
-  auto lidx=lval.find("<!>");
-  if (lidx != std::string::npos) {
-    lval=lval.substr(0,lidx);
-  }
-  auto rval=right;
-  auto ridx=rval.find("<!>");
-  if (ridx != std::string::npos) {
-    rval=rval.substr(0,ridx);
-  }
-  lidx=lval.find(":");
-  ridx=rval.find(":");
-  if (lidx != std::string::npos && ridx != std::string::npos) {
-    if (lval.substr(0,lidx) != rval.substr(0,ridx)) {
-	return strutils::to_lower(lval) < strutils::to_lower(rval);
-    }
-    else {
-	auto lparts=strutils::split(lval.substr(lidx+2),",");
-	auto rparts=strutils::split(rval.substr(ridx+2),",");
-	std::vector<std::string> lvals,lunits,rvals,runits;
-	for (auto part : lparts) {
-	  strutils::trim(part);
-	  auto idx=part.find(" ");
-	  lvals.emplace_back(part.substr(0,idx));
-	  if (idx != std::string::npos) {
-	    lunits.emplace_back(part.substr(idx+1));
-	  }
-	  else {
-	    lunits.emplace_back("");
-	  }
-	}
-	for (auto part : rparts) {
-	  strutils::trim(part);
-	  auto idx=part.find(" ");
-	  rvals.emplace_back(part.substr(0,idx));
-	  if (idx != std::string::npos) {
-	    runits.emplace_back(part.substr(idx+1));
-	  }
-	  else {
-	    runits.emplace_back("");
-	  }
-	}
-	for (size_t n=0; n < lvals.size(); ++n) {
-	  if (strutils::contains(lvals[n],".") || strutils::contains(rvals[n],".")) {
-	    if (!strutils::contains(lvals[n],".")) {
-		lvals[n]+=".";
-	    }
-	    if (!strutils::contains(rvals[n],".")) {
-		rvals[n]+=".";
-	    }
-	    auto lidx=lvals[n].find(".");
-	    auto ridx=rvals[n].find(".");
-	    if (lidx == ridx) {
-		if (lvals[n].length() < 8) {
-		  lvals[n].append(8-lvals[n].length(),'0');
-		}
-		if (rvals[n].length() < 8) {
-		  rvals[n].append(8-rvals[n].length(),'0');
-		}
-	    }
-	    else if (lidx < ridx) {
-		lvals[n].insert(0,ridx-lidx,'0');
-		if (lvals[n].length() < 16) {
-		  lvals[n].append(16-lvals[n].length(),'0');
-		}
-		if (rvals[n].length() < 16) {
-		  rvals[n].append(16-rvals[n].length(),'0');
-		}
-	    }
-	    else {
-		rvals[n].insert(0,lidx-ridx,'0');
-		if (lvals[n].length() < 16) {
-		  lvals[n].append(16-lvals[n].length(),'0');
-		}
-		if (rvals[n].length() < 16) {
-		  rvals[n].append(16-rvals[n].length(),'0');
-		}
-	    }
-	  }
-	  else {
-	    if (lvals[n].length() < 8) {
-		lvals[n].insert(0,8-lvals[n].length(),'0');
-	    }
-	    if (rvals[n].length() < 8) {
-		rvals[n].insert(0,8-rvals[n].length(),'0');
-	    }
-	  }
-	  if ((strutils::contains(lunits[n],"mbar") && strutils::contains(runits[n],"mbar")) || (strutils::contains(lunits[n],"Pa") && strutils::contains(runits[n],"Pa")) || (strutils::contains(lunits[n],"sigma") && strutils::contains(runits[n],"sigma"))) {
-	    if (lvals[n] > rvals[n]) {
-		return true;
-	    }
-	    else if (lvals[n] < rvals[n]) {
-		return false;
-	    }
-	  }
-	  else {
-	    if (lvals[n] < rvals[n]) {
-		return true;
-	    }
-	    else if (lvals[n] > rvals[n]) {
-		return false;
-	    }
-	  }
-	}
-	if ((strutils::contains(lunits.back(),"mbar") && strutils::contains(runits.back(),"mbar")) || (strutils::contains(lunits.back(),"Pa") && strutils::contains(runits.back(),"Pa")) || (strutils::contains(lunits.back(),"sigma") && strutils::contains(runits.back(),"sigma"))) {
-	  return lvals.back() > rvals.back();
-	}
-	else {
-	  return lvals.back() < rvals.back();
-	}
-    }
-  }
-  else {
-    if (lval.empty() && !rval.empty()) {
-	return true;
-    }
-    else if (rval.empty() && !lval.empty()) {
-	return false;
-    }
-    else {
-	return strutils::to_lower(lval) < strutils::to_lower(rval);
-    }
-  }
-}
-
-bool compare_time_ranges(const std::string& left,const std::string& right)
-{
-  std::regex re_anal("Analys[ie]s");
-  if (std::regex_search(left,re_anal) && !std::regex_search(right,re_anal)) {
-    return true;
-  }
-  else if (std::regex_search(right,re_anal) && !std::regex_search(left,re_anal)) {
+  if (rf != string::npos) {
     return false;
   }
-  else {
-    int l_start,r_start;
-    std::string l_end,r_end;
-    std::string l_label,r_label;
-    if (strutils::contains(left,"(initial+")) {
-	auto l_parts=strutils::split(left,"+");
-	auto label_parts=strutils::split(l_parts[0]);
-	l_label=label_parts[label_parts.size()-2];
-	l_start=std::stoi(l_parts[1].substr(0,l_parts[1].find(" ")));
-	l_end=l_parts[2];
-	strutils::chop(l_end);
-    }
-    else if (strutils::contains(left,"-")) {
-	if (strutils::contains(left,"Forecast")) {
-	  l_start=-1;
-	}
-	else {
-	  l_start=0;
-	}
-	l_end=left.substr(0,left.find("-"));
-	auto idx=l_end.rfind(" ");
-	if (idx != std::string::npos) {
-	  l_end=l_end.substr(idx+1);
-	}
-    }
-    else {
-	l_start=0;
-	l_end=left;
-    }
-    if (strutils::contains(right,"(initial+")) {
-	auto r_parts=strutils::split(right,"+");
-	auto label_parts=strutils::split(r_parts[0]);
-	r_label=label_parts[label_parts.size()-2];
-	r_start=std::stoi(r_parts[1].substr(0,r_parts[1].find(" ")));
-	r_end=r_parts[2];
-	strutils::chop(r_end);
-    }
-    else if (strutils::contains(right,"-")) {
-	if (strutils::contains(right,"Forecast")) {
-	  r_start=-1;
-	}
-	else {
-	  r_start=0;
-	}
-	r_end=right.substr(0,right.find("-"));
-	auto idx=r_end.rfind(" ");
-	if (idx != std::string::npos) {
-	  r_end=r_end.substr(idx+1);
-	}
-    }
-    else {
-	r_start=0;
-	r_end=right;
-    }
-    if (l_end.length() < r_end.length()) {
-	l_end.insert(0,r_end.length()-l_end.length(),'0');
-    }
-    if (r_end.length() < l_end.length()) {
-	r_end.insert(0,l_end.length()-r_end.length(),'0');
-    }
-    if (l_end < r_end) {
-	return true;
-    }
-    else if (r_end < l_end) {
-	return false;
-    }
-    else if (l_start < r_start) {
-	return true;
-    }
-    else if (r_start < l_start) {
-	return false;
-    }
-    else {
-	return l_label < r_label;
-    }
+  return left < right;
+}
+
+void standardize_value(string& value) {
+  if (value.find(".") == string::npos) {
+    value += ".";
+  }
+  auto idx = value.find(".");
+  if (idx < 8) {
+    value.insert(0, 8 - idx, '0');
+  }
+  if (value.length() < 17) {
+    value.append(17 - value.length(), '0');
   }
 }
 
-bool default_compare(const std::string& left,const std::string& right)
-{
+bool level_value_compare(string lval, string rval, string lunit, string runit) {
+  if ((lunit.find("mbar") != string::npos && runit.find("mbar") != string::npos)
+      || (lunit.find("Pa") != string::npos && runit.find("Pa")  != string::npos)
+      || (lunit.find("sigma") != string::npos && runit.find("sigma") != string::
+      npos)) {
+    return lval > rval;
+  }
+  return lval < rval;
+}
+
+bool compare_levels(const string& left, const string& right) {
+  auto lval = left;
+  auto lf = lval.find("<!>");
+  if (lf != string::npos) {
+    lval = lval.substr(0, lf);
+  }
+  auto rval = right;
+  auto rf = rval.find("<!>");
+  if (rf != string::npos) {
+    rval = rval.substr(0, rf);
+  }
+  lf = lval.find(":");
+  rf = rval.find(":");
+  if (lf != string::npos && rf != string::npos) {
+    if (lval.substr(0, lf) != rval.substr(0, rf)) {
+      return to_lower(lval) < to_lower(rval);
+    }
+    auto sp_l = split(lval.substr(lf + 2), ",");
+    auto sp_r = split(rval.substr(rf + 2), ",");
+    vector<string> lvals, lunits, rvals, runits;
+    for (auto p : sp_l) {
+      trim(p);
+      auto idx = p.find(" ");
+      lvals.emplace_back(p.substr(0, idx));
+      if (idx != string::npos) {
+        lunits.emplace_back(p.substr(idx + 1));
+      } else {
+        lunits.emplace_back("");
+      }
+    }
+    for (auto p : sp_r) {
+      trim(p);
+      auto idx=p.find(" ");
+      rvals.emplace_back(p.substr(0, idx));
+      if (idx != string::npos) {
+        runits.emplace_back(p.substr(idx + 1));
+      } else {
+        runits.emplace_back("");
+      }
+    }
+    for (size_t n = 0; n < lvals.size(); ++n) {
+      standardize_value(lvals[n]);
+      standardize_value(rvals[n]);
+      if (lvals[n] != rvals[n]) {
+        return level_value_compare(lvals[n], rvals[n], lunits[n], runits[n]);
+      }
+    }
+    return level_value_compare(lvals.back(), rvals.back(), lunits.back(),
+        runits.back());
+  } else {
+    if (lval.empty() && !rval.empty()) {
+      return true;
+    }
+    if (rval.empty() && !lval.empty()) {
+      return false;
+    }
+    return to_lower(lval) < to_lower(rval);
+  }
+}
+
+bool compare_time_ranges(const string& left, const string& right) {
+  regex re_anal("Analys[ie]s");
+  if (regex_search(left, re_anal) && !regex_search(right, re_anal)) {
+    return true;
+  }
+  if (regex_search(right, re_anal) && !regex_search(left, re_anal)) {
+    return false;
+  }
+  int l_start, r_start;
+  string l_end, r_end;
+  string l_label, r_label;
+  if (left.find("(initial+") != string::npos) {
+    auto sp = split(left, "+");
+    auto sp2 = split(sp[0]);
+    l_label = sp2[sp2.size() - 2];
+    l_start = stoi(sp[1].substr(0, sp[1].find(" ")));
+    l_end = sp[2];
+    chop(l_end);
+  } else if (left.find("-") != string::npos) {
+    if (left.find("Forecast") != string::npos) {
+      l_start = -1;
+    } else {
+      l_start = 0;
+    }
+    auto idx = left.find("-");
+    l_end = left.substr(0, idx);
+    l_label = left.substr(idx + 1);
+    idx = l_end.rfind(" ");
+    if (idx != string::npos) {
+      l_end = l_end.substr(idx + 1);
+    }
+  } else {
+    l_start = 0;
+    l_end = left;
+  }
+  if (right.find("(initial+") != string::npos) {
+    auto sp = split(right, "+");
+    auto sp2 = split(sp[0]);
+    r_label = sp2[sp2.size() - 2];
+    r_start = stoi(sp[1].substr(0, sp[1].find(" ")));
+    r_end = sp[2];
+    chop(r_end);
+  } else if (right.find("-") != string::npos) {
+    if (right.find("Forecast") != string::npos) {
+      r_start = -1;
+    } else {
+      r_start = 0;
+    }
+    auto idx = right.find("-");
+    r_end = right.substr(0, idx);
+    r_label = right.substr(idx + 1);
+    idx = r_end.rfind(" ");
+    if (idx != string::npos) {
+      r_end = r_end.substr(idx + 1);
+    }
+  } else {
+    r_start = 0;
+    r_end = right;
+  }
+  if (l_end.length() < r_end.length()) {
+    l_end.insert(0, r_end.length() - l_end.length(), '0');
+  }
+  if (r_end.length() < l_end.length()) {
+    r_end.insert(0, l_end.length() - r_end.length(), '0');
+  }
+  if (l_end < r_end) {
+    return true;
+  }
+  if (r_end < l_end) {
+    return false;
+  }
+  if (l_start < r_start) {
+    return true;
+  }
+  if (r_start < l_start) {
+    return false;
+  }
+  return l_label < r_label;
+}
+
+bool default_compare(const string& left, const string& right) {
   if (left.empty()) {
     return true;
   }
-  else if (right.empty()) {
+  if (right.empty()) {
     return false;
   }
-  else {
-    if (strutils::to_lower(left) <= strutils::to_lower(right)) {
-	return true;
-    }
-    else {
-	return false;
-    }
-  }
+  return to_lower(left) < to_lower(right);
 }
 
 } // end namespace metacompares
