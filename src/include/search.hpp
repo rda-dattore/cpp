e02803b681659909ab8f0d53621ff60e1aa1e29f
rda-dattore <dattore@ucar.edu>
2023-04-19 09:42:50 -0600

    restyled

--- src/include/search.hpp
+++ src/include/search.hpp
@@ -2,15 +2,23 @@
 
 namespace searchutils {
 
-extern std::string cleaned_search_word(std::string& word,bool& ignore);
-extern std::string convert_to_expandable_summary(std::string summary,size_t visible_length,size_t& iterator);
-extern std::string horizontal_resolution_keyword(double max_horizontal_resolution,size_t resolution_type);
-extern std::string index_variables(MySQL::Server& server,std::string dsnum);
-extern std::string index_locations(MySQL::Server& server,std::string dsnum);
-extern std::string insert_word_into_search_wordlist(MySQL::Server& server,const std::string& table,std::string dsnum,std::string word,size_t& location);
-extern std::string root_of_word(std::string word);
-extern std::string time_resolution_keyword(std::string frequency_type,int number,std::string unit,std::string statistics);
 
-extern bool is_compound_term(std::string word,std::string& separator);
+extern std::string cleaned_search_word(std::string& word, bool& ignore);
+extern std::string convert_to_expandable_summary(std::string summary, size_t
+    visible_length, size_t& iterator);
+extern std::string horizontal_resolution_keyword(double
+    max_horizontal_resolution, size_t resolution_type);
+extern std::string root_of_word(std::string word);
+extern std::string time_resolution_keyword(std::string frequency_type, int
+    number, std::string unit, std::string statistics);
+
+extern bool index_locations(MySQL::Server& server, std::string dsnum, std::
+    string& error);
+extern bool indexed_variables(MySQL::Server& server, std::string dsnum, std::
+    string& error);
+extern bool inserted_word_into_search_wordlist(MySQL::Server& server, const
+    std::string& table, std::string dsnum, std::string word, size_t& location,
+    std::string& error);
+extern bool is_compound_term(std::string word, std::string& separator);
 
 } // end namespace searchutils
