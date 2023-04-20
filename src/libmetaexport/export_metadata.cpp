a4c53e9b1892a5276873b3aae7c20291cc9e8509
rda-dattore <dattore@ucar.edu>
2023-04-20 09:38:18 -0600

    restyled

--- src/libmetaexport/export_metadata.cpp
+++ src/libmetaexport/export_metadata.cpp
@@ -16,55 +16,65 @@
 #include <tokendoc.hpp>
 #include <myerror.hpp>
 
+using std::regex;
+using std::regex_search;
+using std::string;
+using std::stringstream;
+using std::unique_ptr;
+
 namespace metadataExport {
 
-bool export_metadata(std::string format,std::unique_ptr<TokenDocument>& token_doc,std::ostream& ofs,std::string ident,size_t initial_indent_length)
-{
-  auto exported=false;
+extern "C" string bind_export_metadata(string format, string ident, size_t
+    initial_indent_length) {
+  unique_ptr<TokenDocument> u;
+  stringstream ss;
+  export_metadata(format, u, ss, ident, initial_indent_length);
+  return ss.str();
+}
+
+bool export_metadata(string format, unique_ptr<TokenDocument>& token_doc, std::
+    ostream& ofs, string ident, size_t initial_indent_length) {
+  auto exported = false;
   TempDir temp_dir;
   temp_dir.create("/tmp");
-  if (std::regex_search(ident,std::regex("^[0-9][0-9][0-9]\\.[0-9]$"))) {
-    std::string ds_overview;
+  if (regex_search(ident, regex("^[0-9][0-9][0-9]\\.[0-9]$"))) {
+    string ds_overview;
     struct stat buf;
-    if (stat((metautils::directives.server_root+"/web/datasets/ds"+ident+"/metadata/dsOverview.xml").c_str(),&buf) == 0) {
-	ds_overview=metautils::directives.server_root+"/web/datasets/ds"+ident+"/metadata/dsOverview.xml";
-    }
-    else {
-	ds_overview=unixutils::remote_web_file("https://rda.ucar.edu/datasets/ds"+ident+"/metadata/dsOverview.xml",temp_dir.name());
+    if (stat((metautils::directives.server_root + "/web/datasets/ds" + ident +
+        "/metadata/dsOverview.xml").c_str(),&buf) == 0) {
+      ds_overview = metautils::directives.server_root + "/web/datasets/ds" +
+          ident + "/metadata/dsOverview.xml";
+    } else {
+      ds_overview = unixutils::remote_web_file("https://rda.ucar.edu/datasets/"
+          "ds" + ident + "/metadata/dsOverview.xml", temp_dir.name());
     }
     XMLDocument xdoc(ds_overview);
     if (xdoc.is_open()) {
-	if (format == "oai_dc") {
-	  exported=export_to_oai_dc(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "datacite") {
-	  exported=export_to_datacite(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "dc-meta-tags") {
-	  exported=export_to_dc_meta_tags(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "dif") {
-	  exported=export_to_dif(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "fgdc") {
-	  exported=export_to_fgdc(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "iso19139") {
-	  exported=export_to_iso19139(token_doc,ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "iso19115-3") {
-	  exported=export_to_iso19115_3(token_doc,ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "json-ld") {
-	  exported=export_to_json_ld(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "native") {
-	  exported=export_to_native(ofs,ident,xdoc,initial_indent_length);
-	}
-	else if (format == "thredds") {
-	  exported=export_to_thredds(ofs,ident,xdoc,initial_indent_length);
-	}
-	xdoc.close();
+      if (format == "oai_dc") {
+        exported = export_to_oai_dc(ofs, ident, xdoc, initial_indent_length);
+      } else if (format == "datacite") {
+        exported = export_to_datacite(ofs, ident, xdoc, initial_indent_length);
+      } else if (format == "dc-meta-tags") {
+        exported = export_to_dc_meta_tags(ofs, ident, xdoc,
+            initial_indent_length);
+      } else if (format == "dif") {
+        exported = export_to_dif(ofs, ident, xdoc, initial_indent_length);
+      } else if (format == "fgdc") {
+        exported = export_to_fgdc(ofs, ident, xdoc, initial_indent_length);
+      } else if (format == "iso19139") {
+        exported = export_to_iso19139(token_doc, ofs, ident, xdoc,
+            initial_indent_length);
+      } else if (format == "iso19115-3") {
+        exported = export_to_iso19115_3(token_doc, ofs, ident, xdoc,
+            initial_indent_length);
+      } else if (format == "json-ld") {
+        exported = export_to_json_ld(ofs, ident, xdoc, initial_indent_length);
+      } else if (format == "native") {
+        exported = export_to_native(ofs, ident, xdoc, initial_indent_length);
+      } else if (format == "thredds") {
+        exported = export_to_thredds(ofs, ident, xdoc, initial_indent_length);
+      }
+      xdoc.close();
     }
   }
   return exported;
