eb8af2526697827072fe217746e18c7814b0ef5d
rda-dattore <dattore@ucar.edu>
2023-09-12 17:26:43 -0600

    updated

--- src/libmetautils/log.cpp
+++ src/libmetautils/log.cpp
@@ -2,74 +2,86 @@
 #include <metadata.hpp>
 #include <utils.hpp>
 
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::regex;
+using std::regex_search;
+using std::string;
+using std::stringstream;
+
 namespace metautils {
 
-void log_info(std::string message,std::string caller,std::string user)
-{
-  std::ofstream log;
-  std::stringstream output,error;
+void log_info(string message, string caller, string user) {
 
-  log.open((directives.rdadata_home+"/logs/md/"+caller+"_log").c_str(),std::fstream::app);
-  log << "user " << user << "/" << directives.host << " at " << dateutils::current_date_time().to_string() << " running with '" << args.args_string << "':" << std::endl;
-  log << message << std::endl;
+  std::ofstream log((directives.rdadata_home + "/logs/md/" + caller + "_log").
+      c_str(), std::fstream::app);
+  log << "user " << user << "/" << directives.host << " at " << dateutils::
+      current_date_time().to_string() << " running with '" << args.args_string
+      << "':" << endl;
+  log << message << endl;
   log.close();
-  unixutils::mysystem2("/bin/chmod 664 "+directives.rdadata_home+"/logs/md/"+caller+"_log",output,error);
+  stringstream oss, ess;
+  unixutils::mysystem2("/bin/chmod 664 " + directives.rdadata_home + "/logs/md/"
+      + caller + "_log", oss, ess);
 }
 
-void log_error(std::string error,std::string caller,std::string user,bool no_exit)
-{
-  size_t idx=0;
-  if (error.substr(0,2) == "-q") {
-    idx=2;
-    error=error.substr(idx);
+void log_error(string error, string caller, string user, bool no_exit) {
+  size_t idx = 0;
+  if (error.substr(0, 2) == "-q") {
+    idx = 2;
+    error = error.substr(idx);
   }
   if (getenv("QUERY_STRING") != nullptr) {
-    std::cout << "Content-type: text/plain" << std::endl << std::endl;
+    cout << "Content-type: text/plain" << endl << endl;
     if (idx == 0) {
-	std::cout << "Error: ";
+	cout << "Error: ";
     }
-    std::cout << error << std::endl;
-  }
-  else {
+    cout << error << endl;
+  } else {
     if (idx == 0) {
-	std::cerr << "Error: ";
+	cerr << "Error: ";
     }
-    std::cerr << error << std::endl;
+    cerr << error << endl;
   }
-  if (!std::regex_search(error,std::regex("^Terminating"))) {
-    log_info(error,caller,user);
+  if (!regex_search(error, regex("^Terminating"))) {
+    log_info(error, caller, user);
   }
   if (!no_exit) {
     exit(1);
   }
 }
 
-void log_error2(std::string error,std::string reporter,std::string caller,std::string user,bool no_exit)
-{
+void log_error2(string error, string reporter, string caller, string user, bool
+    no_exit) {
   if (getenv("QUERY_STRING") != nullptr) {
-    std::cout << "Content-type: text/plain" << std::endl << std::endl;
-    std::cout << error << std::endl;
-  }
-  else {
-    std::cerr << reporter << ": " << error << std::endl;
+    cout << "Content-type: text/plain" << endl << endl;
+    cout << error << endl;
+  } else {
+    cerr << reporter << ": " << error << endl;
   }
 
-// set exit_status = 1 for logging
-// set exit_status = 2 for no logging ("^Terminating" error)
-  auto exit_status=2;
-  if (!std::regex_search(error,std::regex("^Terminating"))) {
-    log_info(reporter+": "+error,caller,user);
-    exit_status=1;
+  // set s = 1 for logging
+  // set s = 2 for no logging ("^Terminating" error)
+  auto s = 2;
+  if (args.dsnum != "999.9" && args.dsnum != "test" && !regex_search(error,
+      regex("^Terminating"))) {
+    log_info(reporter + ": " + error, caller, user);
+    s = 1;
   }
   if (!no_exit) {
-    exit(exit_status);
+    exit(s);
   }
 }
 
-void log_warning(std::string warning,std::string caller,std::string user)
-{
-//  std::cerr << "Warning: " << warning << std::endl;
-  log_info(warning,caller,user);
+void log_error2(string error, string reporter, const UtilityIdentification&
+    util_ident, bool no_exit) {
+  log_error2(error, reporter, util_ident.m_caller, util_ident.m_user, no_exit);
+}
+
+void log_warning(string warning, string caller, string user) {
+//  cerr << "Warning: " << warning << endl;
+  log_info(warning, caller, user);
 }
 
 } // end namespace metautils
