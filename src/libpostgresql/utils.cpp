d7c8166c0fec2be9ab7aeefd3dd90029b4b6331d
rda-dattore <dattore@ucar.edu>
2023-09-29 09:18:21 -0600

    add table_names()

--- src/libpostgresql/utils.cpp
+++ src/libpostgresql/utils.cpp
@@ -2,6 +2,7 @@
 #include <myexception.hpp>
 
 using std::string;
+using std::vector;
 
 namespace PostgreSQL {
 
@@ -13,9 +14,8 @@ bool table_exists(Server& server, string absolute_table) {
   }
   auto schema = absolute_table.substr(0, idx);
   auto tbl = absolute_table.substr(idx + 1);
-  LocalQuery q;
-  q.set("select tablename from pg_catalog.pg_tables where schemaname = '" +
-      schema + "' and tablename like '" + tbl + "'");
+  LocalQuery q("select tablename from pg_catalog.pg_tables where schemaname = '"
+      + schema + "' and tablename like '" + tbl + "'");
   if (q.submit(server) < 0) {
     return false;
   }
@@ -33,9 +33,8 @@ bool field_exists(Server& server, string absolute_table, string field) {
   }
   auto schema = absolute_table.substr(0, idx);
   auto tbl = absolute_table.substr(idx + 1);
-  LocalQuery q;
-  q.set("select column_name from information_schema.columns where table_schema "
-      "= '" + schema + "' and table_name = '" + tbl + "'");
+  LocalQuery q("select column_name from information_schema.columns where "
+      "table_schema = '" + schema + "' and table_name = '" + tbl + "'");
   if (q.submit(server) < 0) {
     return false;
   }
@@ -45,6 +44,22 @@ bool field_exists(Server& server, string absolute_table, string field) {
     }
   }
   return false;
+}
+
+vector<string> table_names(Server& server, string schema, string like_expr,
+    string& error) {
+  vector<string> v; // return value
+  error.clear();
+  LocalQuery q("select tablename from pg_catalog.pg_tables where schemaname = '"
+      + schema + "' and tablename like '" + like_expr + "'");
+  if (q.submit(server) == 0) {
+    for (const auto& r : q) {
+      v.emplace_back(r[0]);
+    }
+  } else {
+    error = q.error();
+  }
+  return v;
 }
 
 } // end namespace PostgreSQL
