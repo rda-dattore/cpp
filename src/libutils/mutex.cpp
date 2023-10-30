fa8d8f46cb3be76da86ffc4e5efb23a47383e78e
rda-dattore <dattore@ucar.edu>
2023-10-30 14:31:31 -0600

    style update

--- src/libutils/mutex.cpp
+++ src/libutils/mutex.cpp
@@ -1,12 +1,10 @@
 #include <mutex.hpp>
 
-void Mutex::lock()
-{
-  while (lock_flag == 1);
-  lock_flag=1;
+void Mutex::lock() {
+  while (lock_flag == 1) { }
+  lock_flag = 1;
 }
 
-void Mutex::unlock()
-{
-  lock_flag=0;
+void Mutex::unlock() {
+  lock_flag = 0;
 }
