e740ceb02df9204938e3657a2247f8c67cc5ab7b
rda-dattore <dattore@ucar.edu>
2024-07-26 21:16:59 -0600

    MySQL -> PostgreSQL; update for new dsid scheme

--- src/libmetaexport/export_metadata.cpp
+++ src/libmetaexport/export_metadata.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <regex>
 #include <sys/stat.h>
 #include <pthread.h>
-#include <metadata_export.hpp>
+#include <metadata_export_pg.hpp>
 #include <xml.hpp>
-#include <MySQL.hpp>
 #include <bsort.hpp>
 #include <strutils.hpp>
 #include <utils.hpp>
 #include <metadata.hpp>
 #include <metahelpers.hpp>
 #include <gridutils.hpp>
-#include <citation.hpp>
+#include <citation_pg.hpp>
 #include <hereDoc.hpp>
 #include <tokendoc.hpp>
 #include <myerror.hpp>
 
-using std::regex;
-using std::regex_search;
 using std::string;
 using std::stringstream;
 using std::unique_ptr;
@@ -38,44 +34,42 @@ bool export_metadata(string format, unique_ptr<TokenDocument>& token_doc, std::
   auto exported = false;
   TempDir temp_dir;
   temp_dir.create("/tmp");
-  if (regex_search(ident, regex("^[0-9][0-9][0-9]\\.[0-9]$"))) {
-    string ds_overview;
-    struct stat buf;
-    if (stat((metautils::directives.server_root + "/web/datasets/ds" + ident +
-        "/metadata/dsOverview.xml").c_str(),&buf) == 0) {
-      ds_overview = metautils::directives.server_root + "/web/datasets/ds" +
-          ident + "/metadata/dsOverview.xml";
-    } else {
-      ds_overview = unixutils::remote_web_file("https://rda.ucar.edu/datasets/"
-          "ds" + ident + "/metadata/dsOverview.xml", temp_dir.name());
+  string ds_overview;
+  auto fpath = ident + "/metadata/dsOverview.xml";
+  struct stat buf;
+  if (stat((metautils::directives.server_root + "/web/datasets/" + fpath).
+      c_str(),&buf) == 0) {
+    ds_overview = metautils::directives.server_root + "/web/datasets/" + fpath;
+  } else {
+    ds_overview = unixutils::remote_web_file("https://rda.ucar.edu/datasets/" +
+        fpath, temp_dir.name());
+  }
+  XMLDocument xdoc(ds_overview);
+  if (xdoc.is_open()) {
+    replace_all(format, "-", "_");
+    if (format == "oai_dc") {
+      exported = export_to_oai_dc(ofs, ident, xdoc, initial_indent_length);
+    } else if (format.find("datacite") == 0) {
+      exported = export_to_datacite(format.substr(8), ofs, ident, xdoc,
+          initial_indent_length);
+    } else if (format == "dc_meta_tags") {
+      exported = export_to_dc_meta_tags(ofs, ident, xdoc,
+          initial_indent_length);
+    } else if (format == "dif") {
+      exported = export_to_dif(ofs, ident, xdoc, initial_indent_length);
+    } else if (format == "fgdc") {
+      exported = export_to_fgdc(ofs, ident, xdoc, initial_indent_length);
+    } else if (format.find("iso") == 0) {
+      exported = export_to_iso(format.substr(3), token_doc, ofs, ident, xdoc,
+          initial_indent_length);
+    } else if (format == "json_ld") {
+      exported = export_to_json_ld(ofs, ident, xdoc, initial_indent_length);
+    } else if (format == "native") {
+      exported = export_to_native(ofs, ident, xdoc, initial_indent_length);
+    } else if (format == "thredds") {
+      exported = export_to_thredds(ofs, ident, xdoc, initial_indent_length);
     }
-    XMLDocument xdoc(ds_overview);
-    if (xdoc.is_open()) {
-      replace_all(format, "-", "_");
-      if (format == "oai_dc") {
-        exported = export_to_oai_dc(ofs, ident, xdoc, initial_indent_length);
-      } else if (format.find("datacite") == 0) {
-        exported = export_to_datacite(format.substr(8), ofs, ident, xdoc,
-            initial_indent_length);
-      } else if (format == "dc_meta_tags") {
-        exported = export_to_dc_meta_tags(ofs, ident, xdoc,
-            initial_indent_length);
-      } else if (format == "dif") {
-        exported = export_to_dif(ofs, ident, xdoc, initial_indent_length);
-      } else if (format == "fgdc") {
-        exported = export_to_fgdc(ofs, ident, xdoc, initial_indent_length);
-      } else if (format.find("iso") == 0) {
-        exported = export_to_iso(format.substr(3), token_doc, ofs, ident, xdoc,
-            initial_indent_length);
-      } else if (format == "json_ld") {
-        exported = export_to_json_ld(ofs, ident, xdoc, initial_indent_length);
-      } else if (format == "native") {
-        exported = export_to_native(ofs, ident, xdoc, initial_indent_length);
-      } else if (format == "thredds") {
-        exported = export_to_thredds(ofs, ident, xdoc, initial_indent_length);
-      }
-      xdoc.close();
-    }
+    xdoc.close();
   }
   return exported;
 }
