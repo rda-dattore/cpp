914d46eeefd67a30adffd2b4abd00ad8f274d2d5
rda-dattore <dattore@ucar.edu>
2024-10-22 10:01:25 -0600

    updated for type change

--- src/libgrids/print_ascii_grids.cpp
+++ src/libgrids/print_ascii_grids.cpp
@@ -1,6 +1,6 @@
 #include <grid.hpp>
 
-int printASCIIGrids(std::string input_filename,size_t format,size_t start,size_t stop)
+int printASCIIGrids(std::string input_filename, Grid::Format format,size_t start,size_t stop)
 {
   idstream *grid_stream;
   Grid *source_grid;
@@ -10,69 +10,58 @@ int printASCIIGrids(std::string input_filename,size_t format,size_t start,size_t
   unsigned char buffer[MAX_LEN];
 
   switch (format) {
-    case Grid::gribFormat:
-    {
+    case Grid::Format::grib: {
 	grid_stream=new InputGRIBStream;
 	source_grid=new GRIBGrid;
 	break;
     }
-    case Grid::tdlgribFormat:
-    {
+    case Grid::Format::tdlgrib: {
 	grid_stream=new InputTDLGRIBGridStream;
 	source_grid=new TDLGRIBGrid;
 	break;
     }
-    case Grid::octagonalFormat:
-    {
+    case Grid::Format::octagonal: {
 	grid_stream=new InputOctagonalGridStream;
 	source_grid=new OctagonalGrid;
 	break;
     }
-    case Grid::latlonFormat:
-    {
+    case Grid::Format::latlon: {
 	grid_stream=new InputLatLonGridStream;
 	source_grid=new LatLonGrid;
 	break;
     }
-    case Grid::latlon25Format:
-    {
+    case Grid::Format::latlon25: {
 	grid_stream=new InputLatLonGridStream(2.5);
 	source_grid=new LatLonGrid(2.5);
 	break;
     }
-    case Grid::slpFormat:
-    {
+    case Grid::Format::slp: {
 	grid_stream=new InputSLPGridStream;
 	source_grid=new SLPGrid;
 	break;
     }
-    case Grid::on84Format:
-    {
+    case Grid::Format::on84: {
 	grid_stream=new InputON84GridStream;
 	source_grid=new ON84Grid;
 	break;
     }
-    case Grid::cgcm1Format:
-    {
+    case Grid::Format::cgcm1: {
 	grid_stream=new InputCGCM1GridStream;
 	source_grid=new CGCM1Grid;
 	break;
     }
-    case Grid::ussrslpFormat:
-    {
+    case Grid::Format::ussrslp: {
 	grid_stream=new InputUSSRSLPGridStream;
 	source_grid=new USSRSLPGrid;
 	break;
     }
-    case Grid::navyFormat:
-    {
+    case Grid::Format::navy: {
 	grid_stream=new InputNavyGridStream;
 	source_grid=new NavyGrid;
 	break;
     }
-    default:
-    {
-	std::cerr << "Error: invalid format specified " << format << std::endl;
+    default: {
+	std::cerr << "Error: invalid format specified " << static_cast<int>(format) << std::endl;
 	return 1;
     }
   }
@@ -88,7 +77,7 @@ int printASCIIGrids(std::string input_filename,size_t format,size_t start,size_t
     }
     file_size+=bytes_read;
     if (grid_stream->number_read() >= start) {
-	source_grid->fill(buffer,Grid::full_grid);
+	source_grid->fill(buffer, Grid::FULL_GRID);
 	source_grid->print_ascii(std::cout);
     }
   }
--- src/libgrids/print_grids.cpp
+++ src/libgrids/print_grids.cpp
@@ -1,98 +1,98 @@
 #include <iomanip>
 #include <grid.hpp>
 
-namespace gridPrint {
+namespace grid_print {
 
-int print(std::string input_filename,size_t format,bool headers_only,bool verbose,size_t start,size_t stop,std::string path_to_parameter_map)
+int print(std::string input_filename, Grid::Format format,bool headers_only,bool verbose,size_t start,size_t stop,std::string path_to_parameter_map)
 {
   idstream *grid_stream;
   Grid *source_grid;
   GRIBMessage *msg=NULL;
   switch (format) {
-    case Grid::cgcm1Format: {
+    case Grid::Format::cgcm1: {
 	grid_stream=new InputCGCM1GridStream;
 	source_grid=new CGCM1Grid;
 	break;
     }
-    case Grid::dslpFormat: {
+    case Grid::Format::dslp: {
 	grid_stream=new InputDiamondSLPGridStream;
 	source_grid=new DiamondSLPGrid;
 	break;
     }
-    case Grid::gribFormat: {
+    case Grid::Format::grib: {
 	grid_stream=new InputGRIBStream;
 	msg=new GRIBMessage;
 	source_grid=new GRIBGrid;
 	break;
     }
-    case Grid::grib2Format: {
+    case Grid::Format::grib2: {
 	grid_stream=new InputGRIBStream;
 	msg=new GRIB2Message;
 	source_grid=new GRIB2Grid;
 	break;
     }
-    case Grid::jraieeemmFormat: {
+    case Grid::Format::jraieeemm: {
 	grid_stream=new InputJRAIEEEMMGridStream;
 	source_grid=new JRAIEEEMMGrid;
 	break;
     }
-    case Grid::latlonFormat: {
+    case Grid::Format::latlon: {
 	grid_stream=new InputLatLonGridStream;
 	source_grid=new LatLonGrid;
 	break;
     }
-    case Grid::latlon25Format: {
+    case Grid::Format::latlon25: {
 	grid_stream=new InputLatLonGridStream(2.5);
 	source_grid=new LatLonGrid(2.5);
 	break;
     }
-    case Grid::navyFormat: {
+    case Grid::Format::navy: {
 	grid_stream=new InputNavyGridStream;
 	source_grid=new NavyGrid;
 	break;
     }
-    case Grid::octagonalFormat: {
+    case Grid::Format::octagonal: {
 	grid_stream=new InputOctagonalGridStream;
 	source_grid=new OctagonalGrid;
 	break;
     }
-    case Grid::oldlatlonFormat: {
+    case Grid::Format::oldlatlon: {
 	grid_stream=new InputOldLatLonGridStream;
 	source_grid=new OldLatLonGrid;
 	break;
     }
-    case Grid::on84Format: {
+    case Grid::Format::on84: {
 	grid_stream=new InputON84GridStream;
 	source_grid=new ON84Grid;
 	break;
     }
-    case Grid::slpFormat: {
+    case Grid::Format::slp: {
 	grid_stream=new InputSLPGridStream;
 	source_grid=new SLPGrid;
 	break;
     }
-    case Grid::tdlgribFormat: {
+    case Grid::Format::tdlgrib: {
 	grid_stream=new InputTDLGRIBGridStream;
 	source_grid=new TDLGRIBGrid;
 	break;
     }
-    case Grid::tropicalFormat: {
+    case Grid::Format::tropical: {
 	grid_stream=new InputTropicalGridStream;
 	source_grid=new TropicalGrid;
 	break;
     }
-    case Grid::ukslpFormat: {
+    case Grid::Format::ukslp: {
 	grid_stream=new InputUKSLPGridStream;
 	source_grid=new UKSLPGrid;
 	break;
     }
-    case Grid::ussrslpFormat: {
+    case Grid::Format::ussrslp: {
 	grid_stream=new InputUSSRSLPGridStream;
 	source_grid=new USSRSLPGrid;
 	break;
     }
     default: {
-	std::cerr << "Error: invalid format specified " << format << std::endl;
+	std::cerr << "Error: invalid format specified " << static_cast<int>(format) << std::endl;
 	return 1;
     }
   }
@@ -102,7 +102,7 @@ int print(std::string input_filename,size_t format,bool headers_only,bool verbos
   }
   for (const auto& byte_stream : *grid_stream) {
     if (grid_stream->number_read() >= start) {
-	if (format == Grid::gribFormat || format == Grid::grib2Format) {
+	if (format == Grid::Format::grib || format == Grid::Format::grib2) {
 	  msg->fill(byte_stream,headers_only);
 	  if (verbose) {
 	    std::cout << "MESSAGE: " << std::setw(6) << grid_stream->number_read();
@@ -123,7 +123,7 @@ int print(std::string input_filename,size_t format,bool headers_only,bool verbos
 	  source_grid->print_header(std::cout,verbose,path_to_parameter_map);
 	}
 	if (!headers_only && verbose) {
-	  if (format == Grid::gribFormat || format == Grid::grib2Format) {
+	  if (format == Grid::Format::grib || format == Grid::Format::grib2) {
 	    for (size_t n=0; n < msg->number_of_grids(); ++n) {
 		source_grid=msg->grid(n);
 		source_grid->set_path_to_gaussian_latitude_data("/glade/u/home/rdadata/share/GRIB");
@@ -142,74 +142,63 @@ int print(std::string input_filename,size_t format,bool headers_only,bool verbos
   return 0;
 }
 
-int print_ascii(std::string input_filename,size_t format,size_t start,size_t stop)
+int print_ascii(std::string input_filename, Grid::Format format,size_t start,size_t stop)
 {
   idstream *grid_stream;
   Grid *source_grid;
   switch (format) {
-    case Grid::gribFormat:
-    {
+    case Grid::Format::grib: {
 	grid_stream=new InputGRIBStream;
 	source_grid=new GRIBGrid;
 	break;
     }
-    case Grid::tdlgribFormat:
-    {
+    case Grid::Format::tdlgrib: {
 	grid_stream=new InputTDLGRIBGridStream;
 	source_grid=new TDLGRIBGrid;
 	break;
     }
-    case Grid::octagonalFormat:
-    {
+    case Grid::Format::octagonal: {
 	grid_stream=new InputOctagonalGridStream;
 	source_grid=new OctagonalGrid;
 	break;
     }
-    case Grid::latlonFormat:
-    {
+    case Grid::Format::latlon: {
 	grid_stream=new InputLatLonGridStream;
 	source_grid=new LatLonGrid;
 	break;
     }
-    case Grid::latlon25Format:
-    {
+    case Grid::Format::latlon25: {
 	grid_stream=new InputLatLonGridStream(2.5);
 	source_grid=new LatLonGrid(2.5);
 	break;
     }
-    case Grid::slpFormat:
-    {
+    case Grid::Format::slp: {
 	grid_stream=new InputSLPGridStream;
 	source_grid=new SLPGrid;
 	break;
     }
-    case Grid::on84Format:
-    {
+    case Grid::Format::on84: {
 	grid_stream=new InputON84GridStream;
 	source_grid=new ON84Grid;
 	break;
     }
-    case Grid::cgcm1Format:
-    {
+    case Grid::Format::cgcm1: {
 	grid_stream=new InputCGCM1GridStream;
 	source_grid=new CGCM1Grid;
 	break;
     }
-    case Grid::ussrslpFormat:
-    {
+    case Grid::Format::ussrslp: {
 	grid_stream=new InputUSSRSLPGridStream;
 	source_grid=new USSRSLPGrid;
 	break;
     }
-    case Grid::navyFormat:
-    {
+    case Grid::Format::navy: {
 	grid_stream=new InputNavyGridStream;
 	source_grid=new NavyGrid;
 	break;
     }
-    default:
-    {
-	std::cerr << "Error: invalid format specified " << format << std::endl;
+    default: {
+	std::cerr << "Error: invalid format specified " << static_cast<int>(format) << std::endl;
 	return 1;
     }
   }
@@ -223,7 +212,7 @@ int print_ascii(std::string input_filename,size_t format,size_t start,size_t sto
 	break;
     }
     if (grid_stream->number_read() >= start) {
-	source_grid->fill(byte_stream,Grid::full_grid);
+	source_grid->fill(byte_stream, Grid::FULL_GRID);
 	source_grid->print_ascii(std::cout);
     }
   }
