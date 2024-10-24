9d12d26d69ca80c9dd542eb341280ae6d9b6f28e
rda-dattore <dattore@ucar.edu>
2024-10-24 08:14:06 -0600

    MySQL -> PostgreSQL; remove HPSS check

--- src/libmetautils/primaryMetadata.cpp
+++ src/libmetautils/primaryMetadata.cpp
@@ -2,106 +2,81 @@
 #include <strutils.hpp>
 #include <utils.hpp>
 #include <bits.hpp>
-#include <MySQL.hpp>
+#include <PostgreSQL.hpp>
+
+using namespace PostgreSQL;
+using std::string;
+using strutils::strget;
+using unixutils::exists_on_server;
 
 namespace metautils {
 
 namespace primaryMetadata {
 
-bool expand_file(std::string dirname,std::string filename,std::string *file_format)
-{
-  if (!std::regex_search(args.data_format,std::regex("^grib"))) {
-// check to see if the file is a tar file
-    auto fp=fopen64(filename.c_str(),"r");
+bool expand_file(string dirname, string filename, string *file_format) {
+  if (args.data_format.find("grib") != 0) {
+
+    // check to see if the file is a tar file
+    auto fp = fopen64(filename.c_str(), "r");
     unsigned char buffer[512];
-    if (fread(buffer,1,512,fp) != 512) {
+    if (fread(buffer, 1, 512, fp) != 512) {
       return false;
     }
     fclose(fp);
-    auto cksum_len=0;
-    for (size_t n=148; n < 156; ++n) {
-	if (((reinterpret_cast<char *>(buffer))[n] >= '0' && (reinterpret_cast<char *>(buffer))[n] <= '9') || (reinterpret_cast<char *>(buffer))[n] == ' ') {
-	  ++cksum_len;
-	}
-	else {
-	  n=156;
-	}
+    auto cksum_len = 0;
+    for (size_t n = 148; n < 156; ++n) {
+      if (((reinterpret_cast<
+          char *>(buffer))[n] >= '0' && (reinterpret_cast<char *>(buffer))[n] <=
+          '9') || (reinterpret_cast<char *>(buffer))[n] == ' ') {
+        ++cksum_len;
+      } else {
+        n = 156;
+      }
     }
     int cksum;
-    strutils::strget(&(reinterpret_cast<char *>(buffer))[148],cksum,cksum_len);
-    auto sum=256;
-    for (size_t n=0; n < 148; ++n) {
-	sum+=static_cast<int>(buffer[n]);
+    strget(&(reinterpret_cast<char *>(buffer))[148], cksum, cksum_len);
+    auto sum = 256;
+    for (size_t n = 0; n < 148; ++n) {
+      sum += static_cast<int>(buffer[n]);
     }
-    for (size_t n=156; n < 512; ++n) {
-	sum+=static_cast<int>(buffer[n]);
+    for (size_t n = 156; n < 512; ++n) {
+      sum += static_cast<int>(buffer[n]);
     }
     if (sum == xtox::otoi(cksum)) {
-	unixutils::untar(dirname,filename);
-	if (file_format != nullptr) {
-	  if (!file_format->empty()) {
-	    (*file_format)+=".";
-	  }
-	  (*file_format)+="TAR";
-	}
-	return true;
+      unixutils::untar(dirname, filename);
+      if (file_format != nullptr) {
+        if (!file_format->empty()) {
+          (*file_format) += ".";
+        }
+        (*file_format) += "TAR";
+      }
+      return true;
     }
   }
-// check to see if the file is gzipped
-  auto fp=fopen64(filename.c_str(),"r");
+
+  // check to see if the file is gzipped
+  auto fp = fopen64(filename.c_str(), "r");
   unsigned char buffer[4];
-  if (fread(buffer,1,4,fp) != 4) {
+  if (fread(buffer, 1, 4, fp) != 4) {
     return false;
   }
   fclose(fp);
   int magic;
-  bits::get(buffer,magic,0,32);
+  bits::get(buffer, magic, 0, 32);
   if (magic == 0x1f8b0808) {
-    if (system(("mv "+filename+" "+filename+".gz; gunzip -f "+filename).c_str()) != 0) {
+    if (system(("mv " + filename + " " + filename + ".gz; gunzip -f " +
+        filename).c_str()) != 0) {
       return false;
     }
     if (file_format != nullptr) {
-	if (!file_format->empty()) {
-	  (*file_format)+=".";
-	}
-	(*file_format)+="GZ";
+      if (!file_format->empty()) {
+        (*file_format) += ".";
+      }
+      (*file_format) += "GZ";
     }
     return true;
   }
   return false;
-}
-
-void match_web_file_to_hpss_primary(std::string url,std::string& metadata_file)
-{
-  MySQL::Server server(directives.database_server,directives.rdadb_username,directives.rdadb_password,"dssdb");
-  MySQL::Query query("select m.mssfile from wfile as w left join mssfile as m on m.mssid = w.mssid where w.dsid = 'ds"+args.dsnum+"' and w.wfile = '"+metautils::relative_web_filename(url)+"' and w.property = 'A' and w.type = 'D' and m.data_size = w.data_size");
-  if (query.submit(server) < 0) {
-    std::cerr << query.error() << std::endl;
-    exit(1);
-  }
-  MySQL::Row row;
-  if (query.fetch_row(row) && !row[0].empty()) {
-    metadata_file=row[0];
-    strutils::replace_all(metadata_file,"/FS/DSS/","");
-    strutils::replace_all(metadata_file,"/DSS/","");
-    strutils::replace_all(metadata_file,"/","%");
-    if (unixutils::exists_on_server(directives.web_server,"/data/web/datasets/ds"+args.dsnum+"/metadata/fmd/"+metadata_file+".GrML",directives.rdadata_home)) {
-	metadata_file+=".GrML";
-    }
-    else if (unixutils::exists_on_server(directives.web_server,"/data/web/datasets/ds"+args.dsnum+"/metadata/fmd/"+metadata_file+".ObML",directives.rdadata_home)) {
-	metadata_file+=".ObML";
-    }
-    else if (unixutils::exists_on_server(directives.web_server,"/data/web/datasets/ds"+args.dsnum+"/metadata/fmd/"+metadata_file+".SatML",directives.rdadata_home)) {
-	metadata_file+=".SatML";
-    }
-    else {
-	metadata_file=row[0];
-    }
-  }
-  else {
-    metadata_file="";
-  }
-  server.disconnect();
 }
 
 } // end namespace primaryMetadata
