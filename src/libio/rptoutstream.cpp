c836e7e06803dc5091a2b3f9096fa2e73b6b23c6
rda-dattore <dattore@ucar.edu>
2026-01-07 18:43:29 -0700

    restyled

--- src/libio/rptoutstream.cpp
+++ src/libio/rptoutstream.cpp
@@ -6,6 +6,9 @@
 #include <utils.hpp>
 #include <bits.hpp>
 
+using std::cerr;
+using std::endl;
+
 irstream& irstream::operator=(const irstream& source)
 {
   return *this;
@@ -24,56 +27,53 @@ void irstream::close()
   }
 }
 
-int irstream::ignore()
-{
+int irstream::ignore() {
   if (file_buf_pos == file_buf_len) {
     if (icosstream != nullptr) {
-	if (icosstream->read(file_buf.get(),8000) == eof) {
-	  return eof;
-	}
-	bits::get(file_buf.get(),_flag,0,4);
-	bits::get(file_buf.get(),block_len,28+_flag*4,32);
-	file_buf_len=(block_len-1)*(60+_flag*4);
-    }
-    else {
-	if (fs.is_open()) {
-	  std::cerr << "Error: no irstream has been opened" << std::endl;
-	  exit(1);
-	}
-	fs.read(reinterpret_cast<char *>(file_buf.get()),8);
-	if (fs.gcount() > 0) {
-	  bits::get(file_buf.get(),_flag,0,4);
-	  if (_flag != 1) {
-	    return error;
-	  }
-	  bits::get(file_buf.get(),block_len,28+_flag*4,32);
-	  file_buf_len=(block_len-1)*(60+_flag*4);
-	  fs.read(reinterpret_cast<char *>(&file_buf[8]),file_buf_len/8);
-	}
-	else {
-	  return eof;
-	}
+      if (icosstream->read(file_buf.get(), 8000) == eof) {
+        return eof;
+      }
+      bits::get(file_buf.get(), _flag, 0, 4);
+      bits::get(file_buf.get(), block_len, (28+_flag*4), 32);
+      file_buf_len = (block_len - 1) * (60 + _flag * 4);
+    } else {
+      if (!fs.is_open()) {
+        cerr << "Error: no irstream has been opened" << endl;
+        exit(1);
+      }
+      fs.read(reinterpret_cast<char *>(file_buf.get()), 8);
+      if (fs.gcount() > 0) {
+        bits::get(file_buf.get(), _flag, 0, 4);
+        if (_flag != 1) {
+          return error;
+        }
+        bits::get(file_buf.get(), block_len, (28+_flag*4), 32);
+        file_buf_len = (block_len - 1) * (60 + _flag * 4);
+        fs.read(reinterpret_cast<char *>(&file_buf[8]), (file_buf_len/8));
+      } else {
+        return eof;
+      }
     }
     ++num_blocks;
-    new_block=true;
+    new_block = true;
     long long sum;
-    if (checksum(file_buf.get(),block_len,60+_flag*4,sum) != 0) {
-	std::cerr << "Warning: checksum error on block number " << num_blocks << std::endl;
+    if (checksum(file_buf.get(), block_len, (60+_flag*4), sum) != 0) {
+      cerr << "Warning: checksum error on block number " << num_blocks << endl;
     }
-    file_buf_pos=60+_flag*4;
+    file_buf_pos = 60 + _flag * 4;
   }
   ++num_read;
   int rptlen;
-  bits::get(file_buf.get(),rptlen,file_buf_pos,12);
-  rptlen*=(60+_flag*4);
-  file_buf_pos+=rptlen;
-  return lroundf(static_cast<float>(rptlen)/8);
+  bits::get(file_buf.get(), rptlen, file_buf_pos, 12);
+  rptlen *= (60 + _flag * 4);
+  file_buf_pos += rptlen;
+  return lroundf(static_cast<float>(rptlen) / 8);
 }
 
 bool irstream::open(std::string filename)
 {
   if (is_open()) {
-    std::cerr << "Error: currently connected to another file stream" << std::endl;
+    cerr << "Error: currently connected to another file stream" << endl;
     exit(1);
   }
 // test for COS-blocked file
@@ -88,7 +88,7 @@ bool irstream::open(std::string filename)
     icosstream.reset(nullptr);
     fs.open(filename.c_str(),std::ios::in);
     if (!fs.is_open()) {
-	return false;
+      return false;
     }
   }
   else {
@@ -106,46 +106,46 @@ int irstream::peek()
 {
   if (file_buf_pos == file_buf_len) {
     if (icosstream != nullptr) {
-	auto status=icosstream->read(file_buf.get(),8000);
-	if (status == eof || status == craystream::eod) {
-	  return status;
-	}
-	bits::get(file_buf.get(),_flag,0,4);
-	bits::get(file_buf.get(),block_len,28+_flag*4,32);
-	if (block_len <= 0 || block_len > 1000) {
-	  return error;
-	}
-	file_buf_len=(block_len-1)*(60+_flag*4);
+      auto status=icosstream->read(file_buf.get(),8000);
+      if (status == eof || status == craystream::eod) {
+        return status;
+      }
+      bits::get(file_buf.get(),_flag,0,4);
+      bits::get(file_buf.get(),block_len,28+_flag*4,32);
+      if (block_len <= 0 || block_len > 1000) {
+        return error;
+      }
+      file_buf_len=(block_len-1)*(60+_flag*4);
     }
     else {
-	if (!fs.is_open()) {
-	  std::cerr << "Error: no irstream has been opened" << std::endl;
-	  exit(1);
-	}
-	fs.read(reinterpret_cast<char *>(file_buf.get()),8);
-	if (fs.gcount() > 0) {
-	  bits::get(file_buf.get(),_flag,0,4);
-	  if (_flag != 1) {
-	    return error;
-	  }
-	  bits::get(file_buf.get(),block_len,28+_flag*4,32);
-	  if (block_len > 8000) {
-	    return error;
-	  }
-	  file_buf_len=(block_len-1)*8;
-	  fs.read(reinterpret_cast<char *>(&file_buf[8]),file_buf_len);
-	  file_buf_len=(block_len-1)*(60+_flag*4);
-	}
-	else {
-	  return eof;
-	}
+      if (!fs.is_open()) {
+        cerr << "Error: no irstream has been opened" << endl;
+        exit(1);
+      }
+      fs.read(reinterpret_cast<char *>(file_buf.get()),8);
+      if (fs.gcount() > 0) {
+        bits::get(file_buf.get(),_flag,0,4);
+        if (_flag != 1) {
+          return error;
+        }
+        bits::get(file_buf.get(),block_len,28+_flag*4,32);
+        if (block_len > 8000) {
+          return error;
+        }
+        file_buf_len=(block_len-1)*8;
+        fs.read(reinterpret_cast<char *>(&file_buf[8]),file_buf_len);
+        file_buf_len=(block_len-1)*(60+_flag*4);
+      }
+      else {
+        return eof;
+      }
     }
     ++num_blocks;
     new_block=true;
     word_count+=block_len;
     long long sum;
     if (checksum(file_buf.get(),block_len,60+_flag*4,sum) != 0) {
-	std::cerr << "Warning: checksum error on block number " << num_blocks << std::endl;
+      cerr << "Warning: checksum error on block number " << num_blocks << endl;
     }
     file_buf_pos=60+_flag*4;
   }
@@ -174,7 +174,7 @@ int irstream::read(unsigned char *buffer,size_t buffer_length)
   }
   else {
     if (buffer_length > 0) {
-	std::copy(&file_buf[file_buf_pos],&file_buf[file_buf_pos+buffer_length],buffer);
+      std::copy(&file_buf[file_buf_pos],&file_buf[file_buf_pos+buffer_length],buffer);
     }
     file_buf_pos+=rptlen;
     return buffer_length;
@@ -204,74 +204,70 @@ void orstream::close()
   file_name="";
 }
 
-int orstream::flush()
-{
+int orstream::flush() {
   if (file_buf_pos > 8) {
-    auto num_to_write=file_buf_pos+8;
-    block_len=num_to_write/8;
-    bits::set(file_buf.get(),1,0,4);
-    bits::set(file_buf.get(),block_len,32,32);
-    word_count+=block_len;
+    auto num_to_write = file_buf_pos+8;
+    block_len = num_to_write/8;
+    bits::set(file_buf.get(), 1, 0, 4);
+    bits::set(file_buf.get(), block_len, 32, 32);
+    word_count += block_len;
     long long sum;
-    checksum(file_buf.get(),block_len,64,sum);
-    bits::set(&file_buf[file_buf_pos],sum,0,64);
-    size_t num_out=fs.tellp();
-    fs.write(reinterpret_cast<char *>(file_buf.get()),num_to_write);
-    num_out=static_cast<size_t>(fs.tellp())-num_out;
+    checksum(file_buf.get(), block_len, 64, sum);
+    bits::set(&file_buf[file_buf_pos], sum, 0, 64);
+    size_t num_out = fs.tellp();
+    fs.write(reinterpret_cast<char *>(file_buf.get()), num_to_write);
+    num_out = static_cast<size_t>(fs.tellp()) - num_out;
     if (num_out != num_to_write) {
-	return error;
+      return error;
     }
     ++num_blocks;
-    file_buf_len=7984;
-    file_buf_pos=8;
-    new_block=true;
+    file_buf_len = 7984;
+    file_buf_pos = 8;
+    new_block = true;
     return 0;
-  }
-  else {
+  } else {
     return 1;
   }
 }
 
-bool orstream::open(std::string filename)
-{
+bool orstream::open(std::string filename) {
   if (is_open()) {
-    std::cerr << "Error: currently connected to another file stream" << std::endl;
+    cerr << "Error: currently connected to another file stream" << endl;
     exit(1);
   }
-  fs.open(filename.c_str(),std::ios::out);
+  fs.open(filename.c_str(), std::ios::out);
   if (!fs.is_open()) {
     return false;
   }
-  file_name=filename;
-  file_buf_len=7984;
-  file_buf_pos=8;
-  num_written=num_blocks=0;
-  word_count=0;
-  new_block=false;
+  file_name = filename;
+  file_buf_len = 7984;
+  file_buf_pos = 8;
+  num_written = 0;
+  num_blocks = 0;
+  word_count = 0;
+  new_block = false;
   return true;
 }
 
-int orstream::write(const unsigned char *buffer,size_t num_bytes)
-{
+int orstream::write(const unsigned char *buffer, size_t num_bytes) {
   if (!fs.is_open()) {
-    std::cerr << "Error: no orstream has been opened" << std::endl;
+    cerr << "Error: no orstream has been opened" << endl;
     exit(1);
   }
   if (num_bytes > file_buf_len) {
     if (orstream::flush() == error) {
-	return error;
+      return error;
     }
-  }
-  else {
-    new_block=false;
+  } else {
+    new_block = false;
   }
   ++num_written;
-  auto num_words=(num_bytes+7)/8;
-  auto num_chars=num_words*8;
-  std::copy(&buffer[0],&buffer[num_chars],&file_buf[file_buf_pos]);
-  bits::set(&file_buf[file_buf_pos],num_words,0,12);
-  file_buf_len-=num_chars;
-  file_buf_pos+=num_chars;
+  auto num_words = (num_bytes+7) / 8;
+  auto num_chars = num_words * 8;
+  std::copy(&buffer[0], &buffer[num_chars], &file_buf[file_buf_pos]);
+  bits::set(&file_buf[file_buf_pos], num_words, 0, 12);
+  file_buf_len -= num_chars;
+  file_buf_pos += num_chars;
   return num_bytes;
 }
 
@@ -294,9 +290,10 @@ void ocrstream::close()
   ocosstream.reset(nullptr);
 }
 
-int ocrstream::flush()
-{
+int ocrstream::flush() {
+cerr << "FLUSH-A" << endl;
   if (file_buf_pos > 8) {
+cerr << "FLUSH-B" << endl;
     size_t num_out=file_buf_pos+8;
     block_len=num_out/8;
     bits::set(file_buf.get(),1,0,4);
@@ -306,15 +303,14 @@ int ocrstream::flush()
     checksum(file_buf.get(),block_len,64,sum);
     bits::set(&file_buf[file_buf_pos],sum,0,64);
     if (ocosstream->write(file_buf.get(),num_out) <= 0) {
-	return error;
+      return error;
     }
     ++num_blocks;
     file_buf_len=7984;
     file_buf_pos=8;
     new_block=true;
     return 0;
-  }
-  else {
+  } else {
     return 1;
   }
 }
@@ -322,7 +318,7 @@ int ocrstream::flush()
 bool ocrstream::open(std::string filename)
 {
   if (is_open()) {
-    std::cerr << "Error: currently connected to another file stream" << std::endl;
+    cerr << "Error: currently connected to another file stream" << endl;
     exit(1);
   }
   ocosstream.reset(new ocstream);
@@ -342,12 +338,12 @@ bool ocrstream::open(std::string filename)
 int ocrstream::write(const unsigned char *buffer,size_t num_bytes)
 {
   if (ocosstream == nullptr) {
-    std::cerr << "Error: no ocrstream has been opened" << std::endl;
+    cerr << "Error: no ocrstream has been opened" << endl;
     exit(1);
   }
   if (num_bytes > file_buf_len) {
     if (ocrstream::flush() == error) {
-	return error;
+      return error;
     }
   }
   else {
@@ -366,7 +362,7 @@ int ocrstream::write(const unsigned char *buffer,size_t num_bytes)
 void ocrstream::write_eof()
 {
   if (ocrstream::flush() == error) {
-    std::cerr << "Warning: error while flushing output rptoutstream" << std::endl;
+    cerr << "Warning: error while flushing output rptoutstream" << endl;
   }
   ocosstream->write_eof();
 }
@@ -392,7 +388,7 @@ int iocrstream::flush()
   switch(stream_type) {
     case 1:
     {
-	return ocrstream::flush();
+      return ocrstream::flush();
     }
   }
   return 2;
@@ -403,11 +399,11 @@ size_t iocrstream::block_length() const
   switch (stream_type) {
     case 0:
     {
-	return irstream::block_len;
+      return irstream::block_len;
     }
     case 1:
     {
-	return ocrstream::block_len;
+      return ocrstream::block_len;
     }
   }
   return 0;
@@ -422,7 +418,7 @@ int iocrstream::ignore()
     }
     case 1:
     {
-      std::cerr << "Warning: ignore should not be called on a stream opened for writing" << std::endl;
+      cerr << "Warning: ignore should not be called on a stream opened for writing" << endl;
     }
   }
   return bfstream::error;
@@ -433,11 +429,11 @@ bool iocrstream::is_new_block() const
   switch (stream_type) {
     case 0:
     {
-	return irstream::new_block;
+      return irstream::new_block;
     }
     case 1:
     {
-	return ocrstream::new_block;
+      return ocrstream::new_block;
     }
   }
   return false;
@@ -445,7 +441,7 @@ bool iocrstream::is_new_block() const
 
 bool iocrstream::open(std::string filename)
 {
-  std::cerr << "Error: no mode given" << std::endl;
+  cerr << "Error: no mode given" << endl;
   return false;
 }
 
@@ -460,7 +456,7 @@ bool iocrstream::open(std::string filename,std::string mode)
     return ocrstream::open(filename);
   }
   else {
-    std::cerr << "Error: bad mode " << mode << std::endl;
+    cerr << "Error: bad mode " << mode << endl;
     return false;
   }
 }
@@ -474,7 +470,7 @@ int iocrstream::peek()
     }
     case 1:
     {
-      std::cerr << "Warning: peek should not be called on a stream opened for writing" << std::endl;
+      cerr << "Warning: peek should not be called on a stream opened for writing" << endl;
       exit(1);
     }
   }
@@ -490,7 +486,7 @@ int iocrstream::read(unsigned char *buffer,size_t buffer_length)
     }
     case 1:
     {
-      std::cerr << "Warning: read should not be called on a stream opened for writing" << std::endl;
+      cerr << "Warning: read should not be called on a stream opened for writing" << endl;
       exit(1);
     }
   }
@@ -518,7 +514,7 @@ int iocrstream::write(const unsigned char *buffer,size_t num_bytes)
   switch (stream_type) {
     case 0:
     {
-      std::cerr << "Warning: write should not be called on a stream opened for reading" << std::endl;
+      cerr << "Warning: write should not be called on a stream opened for reading" << endl;
       exit(1);
     }
     case 1:
@@ -534,13 +530,13 @@ void iocrstream::write_eof()
   switch (stream_type) {
     case 0:
     {
-	std::cerr << "Warning: write_eof should not be called on a stream opened for reading" << std::endl;
-	exit(1);
+      cerr << "Warning: write_eof should not be called on a stream opened for reading" << endl;
+      exit(1);
     }
     case 1:
     {
-	ocrstream::write_eof();
-	break;
+      ocrstream::write_eof();
+      break;
     }
   }
 }
