494c8ef0c3f01f1769e487ca1931d0dbc6cdf74e
rda-dattore <dattore@ucar.edu>
2026-07-01 06:28:58 -0600

    restyled

--- include/observation.hpp
+++ include/observation.hpp
@@ -7,20 +7,21 @@
 
 class Observation {
 public:
-  static const bool header_only,full_report;
+  static const bool header_only, full_report;
   struct ObservationLocation {
-    ObservationLocation() : ID(),latitude(0.),longitude(0.),elevation(0) {}
+    ObservationLocation() : ID(), latitude(0.), longitude(0.), elevation(0) { }
 
     std::string ID;
-    float latitude,longitude;
+    float latitude, longitude;
     union {
 	int elevation;
 	int altitude;
     };
   };
-  Observation() : date_time_(),location_() {}
+  Observation() : date_time_(), location_() { }
   virtual ~Observation() {}
-  virtual void fill(const unsigned char *stream_buffer,bool fill_header_only) =0;
+  virtual void fill(const unsigned char *stream_buffer, bool
+      fill_header_only) =0;
   virtual DateTime date_time() const =0;
   virtual ObservationLocation location() const =0;
 
@@ -29,7 +30,9 @@ protected:
   ObservationLocation location_;
 };
 
-extern bool operator==(const Observation::ObservationLocation& loc1,const Observation::ObservationLocation& loc2);
-extern bool operator!=(const Observation::ObservationLocation& loc1,const Observation::ObservationLocation& loc2);
+extern bool operator==(const Observation::ObservationLocation& loc1, const
+    Observation::ObservationLocation& loc2);
+extern bool operator!=(const Observation::ObservationLocation& loc1, const
+    Observation::ObservationLocation& loc2);
 
 #endif
