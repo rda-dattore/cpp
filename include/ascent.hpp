75ab64bad88cf26af4cd6fdf23a200dc92b4ac3c
rda-dattore <dattore@ucar.edu>
2026-07-01 11:59:10 -0600

    initial commit

---
+++ include/ascent.hpp
@@ -0,0 +1,32 @@
+// FILE: ascent.h
+
+#ifndef ASCENT_H
+#define  ASCENT_H
+
+#include <iodstream.hpp>
+#include <observation.hpp>
+
+class InputASCENTStream : public bfstream {
+public:
+  InputASCENTStream() { }
+  void close();
+  int ignore();
+  bool open(std::string filename);
+  int peek();
+  int read(unsigned char *buffer, size_t buffer_length);
+  void rewind();
+
+private:
+};
+
+class ASCENTData : public Observation {
+public:
+  ASCENTData() { }
+  DateTime date_time() const { return date_time_; }
+  void fill(const unsigned char *stream_buffer, bool fill_header_only);
+  ObservationLocation location() const { return location_; }
+
+private:
+};
+
+#endif
