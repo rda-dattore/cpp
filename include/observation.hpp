f4fde28f5baf143acd28bf67780fb67a912985a9
rda-dattore <dattore@ucar.edu>
2026-07-01 21:29:16 -0600

    update default values

--- include/observation.hpp
+++ include/observation.hpp
@@ -9,7 +9,8 @@ class Observation {
 public:
   static const bool header_only, full_report;
   struct ObservationLocation {
-    ObservationLocation() : ID(), latitude(0.), longitude(0.), elevation(0) { }
+    ObservationLocation() : ID(), latitude(-99.9), longitude(-999.9),
+        elevation(-9999) { }
 
     std::string ID;
     float latitude, longitude;
@@ -19,10 +20,10 @@ public:
     };
   };
   Observation() : date_time_(), location_() { }
-  virtual ~Observation() {}
+  virtual ~Observation() { }
+  virtual DateTime date_time() const =0;
   virtual void fill(const unsigned char *stream_buffer, bool
       fill_header_only) =0;
-  virtual DateTime date_time() const =0;
   virtual ObservationLocation location() const =0;
 
 protected:
